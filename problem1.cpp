341f8dcdf47666a44a0f747b2607687ecf7a48d6
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-13 04:10:05 +0530

    the task wwas to develop to find the largest number from the list.

---
+++ problem1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// the task wwas to develop to find the largest number from the list. 
+#include <vector>
+int main() {
+    std::vector<int> given = {22,5,678,10000};
+    int highest = given[0];
+    for (int x: given) {
+        if (x>highest) {
+            highest=x;
+            std::cout<<"higher is "<< x<<std::endl;
+        } else {
+            std::cout<<"the next number is not greater";
+        }
+    }
+    std::cout<< highest;
+}
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+#include <vector>
+int main() {
+    int deva =  4;
+    deva = 6;
+    std::cout<<deva;
+}
