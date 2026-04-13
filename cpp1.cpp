6afe5576d122f5dcc2ce19ec02effcfb47b906ce
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-13 14:47:23 +0530

    size of data understanding

---
+++ cpp1.cpp
@@ -0,0 +1,8 @@
+#include <iostream > 
+int main() {
+    std::string x= "98564";
+    std::cout<< x <<std::endl;
+    std::cout<< "the size of data is";
+    std::cout<< sizeof(x);
+      
+}
---
+++ size_of_Data.cpp
@@ -0,0 +1,15 @@
+#include <iostream > 
+#include <vector>
+// int main() {
+//     long long int x= 95965216500586220;
+//     std::cout<< "the size of data is: ";
+//     std::cout<< sizeof(x)<<std::endl;
+// }
+// if i wanna fill it with 64 bits of data only
+int main() {
+// veruy large input
+    // int64_t data = 99999999999999999999999999999999999999999999999999999999999999999;
+    double nasdaq= 3.01212;//  used to input decimals
+    std::cout<< nasdaq << std::endl; //  to use float in the code
+
+}
