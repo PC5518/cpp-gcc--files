4a681909bf3bb6364ce37eb0e79d188624f5a312
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-13 14:55:44 +0530

    basics of cpp

---
+++ casting.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// nothing but converting between integer and float value   
+int main() {
+    double integer=-6.25;
+    double deva =  double(integer);
+    integer= int(integer);
+    std::cout<< integer;
+}
