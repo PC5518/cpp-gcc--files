eca60ed2b7925b169d671493df9c998649da5404
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-17 01:24:10 +0530

    accesing the elments of a array in python

---
+++ arraytry.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    int deva[100];
+    for (int i=0; i<100; i++) {
+        deva[i]=i;
+    }
+    for (int i=0; i<100; i++) {
+        std::cout<<deva[i]+1<<std::endl;
+    }// but you can access it one by one via index
+    std::cout<< deva; // you can't access a list at once
+
+}
+// 
