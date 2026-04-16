f97553cd6e6d13d68ca2d64794d76ce98f5180fb
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-17 00:12:28 +0530

    concept of scope

--- scope.cpp
+++ scope.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+int x=7;
 int main() {
-    
+    {
+        int x=7;
+        std::cout<<x<<std::endl;
+    }
+    std::cout<< x;
 }
+// A variable is accessible only from its point of declaration to the closing } of the block it was declared in.
+
