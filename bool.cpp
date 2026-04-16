5ddefcf4da5105e6cb9981177b6debbfdcc63f5e
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-16 18:27:55 +0530

    final boolean knowledge

--- bool.cpp
+++ bool.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-
 int main() {
-    bool deva;
-    
-    // 1. To take input (Type 1 for true, 0 for false)
-    std::cout << "Enter 1 or 0: ";
-    std::cin >> deva; 
-
-    // 2. To see words like "true" instead of "1"
-    std::cout << std::boolalpha;
-
-    if (deva == true) {
-        std::cout << "The value is: " << deva << std::endl;
+    bool aditya= true;
+    if (aditya==true) {
+        std::cout<< "it's true";
     } else {
-        std::cout << "The value is: " << deva << std::endl;
+        std::cout << "it's false";
     }
-
-    return 0;
 }
