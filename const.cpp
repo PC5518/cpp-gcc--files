a5546c1f55d3a2c1895e3c05bfc478d36b96c0a0
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-16 21:31:38 +0530

    const usage in real life programming

---
+++ const.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//  cosnt -> means the value can't be modified
+int main() {
+    const int deva = 7;
+    deva =4; 
+    std::cout::<< deva;
+    // chaning or reassigning a new  value is not allowed
+    // const ensures the var is non changeble
+}
