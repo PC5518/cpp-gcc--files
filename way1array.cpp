2c69c17de504f3d89dd5c8bfc720179749733636
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-21 15:26:36 +0530

    way one is to make an vector @!!

---
+++ way1array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+#include <vector>
+int main() {
+    std::vector<int> vector_array= {1,2,5,25,4};
+    for (int i=0;i< vector_array.size(); i++ ) {
+        std::cout<< vector_array[i]<< " " ;
+    }
+    return 0; 
+}
+# to form an array
