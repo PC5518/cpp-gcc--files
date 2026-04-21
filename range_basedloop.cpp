d3ad72b0118207ea3c762aad60404e29f10a998b
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-21 16:20:43 +0530

    for i in variable: print(i) cpp version

---
+++ range_basedloop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    int deva[]= {1,2,5,80};
+    for (int i:deva ) {
+        std::cout<<i << " ";
+    }
+}
+// variable= [1,2,4,1]
+// cpp version for for i in variable:
+//              print(i)
