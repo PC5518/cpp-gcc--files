e52d87be2a7ecc70f2e247b33bb13b2403611352
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-21 16:06:59 +0530

    way3 of formign a array data structure

---
+++ way3array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <array>
+int main() {
+    std::array<int,4    >  deva ={1,2,5,20};
+    for (int i=0; i < deva.size(); i++ ) {
+        std::cout << deva[i] << " ";
+    }
+}
