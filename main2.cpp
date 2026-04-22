e6eed290ec42ca40c6fa9f4a956cee25adaba58a
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-22 22:17:07 +0530

    basic program

---
+++ main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    int passenger;
+    std::cin>> passenger;
+    if  (passenger==58690) {
+        std::cout<< "door open";
+    } else {
+        std::cout<< "wrong tickert";
+    }
+
+}
+// basics program
