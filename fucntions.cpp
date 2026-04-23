e1b843ff51938ba0fc37937a4d3fdefdf5fd46e1
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-23 20:31:25 +0530

    basics of fxns

---
+++ fucntions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int sum(int a, int b) {
+    return a+b;
+}
+int main() {
+    std::cout<< sum(4,8) << std::endl;
+    return 0;
+}
+// arguments are 1 and 2 
+// a=4, b=8
+// note:  the order matters the point where we are writting the fucntion 
