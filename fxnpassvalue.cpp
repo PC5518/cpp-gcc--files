a7959556ec6fa06abc38c90444323119cf4cf9c4
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-24 23:49:58 +0530

    overall final approach to outisder fxn role in changing the value in the original fxn

--- fxnpassvalue.cpp
+++ fxnpassvalue.cpp
@@ -11,3 +11,5 @@ int main() {
     std::cout<< "x's value is: "<<x <<std::endl;
     std::cout<<"x's adress is: " <<&x <<std::endl;
 }
+// so overall the x value in main fxn value doesn't change . 
+// an outsider defined fxn can't change the value. However it can be used temporarily in  the fxn 
