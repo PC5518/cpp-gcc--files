b76ce41718ff4a5174379ef8f9608409ea0c0d94
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-25 00:29:17 +0530

    ek hi cheez of passing a varible  via  a fxn

---
+++ references.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    int x=748;
+    int& ref =x;
+    std::cout<<"x's value is " << x<<std::endl;
+    std::cout<<"x's adress is " << &x<<std::endl;
+
+
+    std::cout<<"X's adress is " << ref<<std::endl;
+    std::cout<<"x's adress is " << &ref<<std::endl;
+}
