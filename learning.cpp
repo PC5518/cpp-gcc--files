8802e458748830c8dbd8a658e3b0558320399d19
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-25 20:20:47 +0530

    learning the referencees

---
+++ learning.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    std::string x="deva";
+    std::string y="aditya";
+    std::string temp=x;
+    std::string temp2=y;
+    x=temp2;
+    y=temp;
+    std::cout<<"x" <<temp2 <<std::endl;
+    std::cout<<"y"<<temp << std::endl;
+}
