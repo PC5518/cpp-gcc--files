bd007ac3c649b8ad842792f763509eda21459227
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-28 22:52:25 +0530

    claude ai reference test

--- pbr3.cpp
+++ pbr3.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 void mathStats(int a, int b , int& sum, int& product, int& difference) {
-    std::cout<<"sum" << a+b<< std::endl;
-    std::cout<<"product" << a*b<< std::endl;
-    std::cout<<"difference" << a-b<< std::endl
-
+    sum =a+b;
+    product=a*b;
+    difference=a-b;
 }
 int main() {
-    int main()
+    int sum ;
+    int product;
+    int difference;
 
+    std::cout<<sum <<std::endl;
+
+
+    mathStats(12,55, sum, product, difference);
+    std::cout<<sum <<std::endl;
+    std::cout<<difference<<std::endl;
+    std::cout<<product<<std::endl;
 }
