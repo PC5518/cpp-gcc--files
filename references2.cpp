def21f2b3fea8e05f3bdcc2bd4c77d9c3c16933c
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-28 17:12:04 +0530

    first understanding about references

--- references2.cpp
+++ references2.cpp
@@ -3,7 +3,16 @@ int main() {
     int x=4;
     int y=28;
     // now let's use ref to swap up the value 
-    int& a = x;
+    // x and y are two boxes with values
+    // int& a = x means we put a DHL label 'a' on x's box
+    // now x and y have two labels each: x,a and y,b
+    // ek teer do nishane — shoot through a, x gets hit
+    // whatever we do to a, we do to x — same box, always
+
+    int& a = x; // naya var a mat banana just wrap kardo a me i.e ek hi hai dono
+
     int& b= y;
+    // what's the advantage??? 
     
+
 }
