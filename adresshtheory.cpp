cac9a6ca2646339f9ddd31d42696afbdfc2f91fc
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-28 23:50:23 +0530

    first intro to pointer

--- adresshtheory.cpp
+++ adresshtheory.cpp
@@ -4,6 +4,5 @@ int main() {
     auto aditya = (void*)&deva; // it means adress only store karna type mat dekhna
     auto devansh= (int*)&deva;// compiler kom bata diya ki type hai int
     std::cout<<aditya<<std::endl;
-
     std::cout<<devansh<<std::endl;
 }
