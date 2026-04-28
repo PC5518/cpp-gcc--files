10be16ebf044103ef998d5c4d4815f7b64533429
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-28 22:45:12 +0530

    final commit

---
+++ pbr3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+void mathStats(int a, int b , int& sum, int& product, int& difference) {
+    std::cout<<"sum" << a+b<< std::endl;
+    std::cout<<"product" << a*b<< std::endl;
+    std::cout<<"difference" << a-b<< std::endl
+
+}
+int main() {
+    int main()
+
+}
