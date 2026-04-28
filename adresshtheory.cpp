046a0a1a8473b0a098cd3404337cd88ce7c3dc3a
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-04-28 23:13:40 +0530

    void ko change bhi kar sakte hai

--- adresshtheory.cpp
+++ adresshtheory.cpp
@@ -2,6 +2,8 @@
 int main() {
     int deva =  39;
     auto aditya = (void*)&deva; // it means adress only store karna type mat dekhna
-    
-    std::cout<<aditya;
+    auto devansh= (int*)&deva;// compiler kom bata diya ki type hai int
+    std::cout<<aditya<<std::endl;
+
+    std::cout<<devansh<<std::endl;
 }
