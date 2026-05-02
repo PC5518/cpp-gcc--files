eb67e53480fe4c457b39ba52dd6a2f0b6e229918
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-02 23:52:55 +0530

    finally i learned class

--- classess in cpp/class.cpp
+++ classess in cpp/class.cpp
@@ -7,12 +7,12 @@ public:
     int age ;
     int maths;
     int physics;
-    void highest(void) {
+    void highest() {
         if (maths> physics)  {
-            std::cout <<name  << " Highest number is " <<maths  <<std::endl;
+            std::cout <<name  << " Highest number is in maths: " <<maths  <<std::endl;
 
         } else  {
-            std::cout<< name<< " Highest number is " << physics << std::endl;
+            std::cout<< name<< " Highest number is physics " << physics << std::endl;
         }
     }
 
@@ -25,6 +25,13 @@ int main() {
     first_student.maths=36;
     first_student.roll_number=1979;
     first_student.physics=78;
-    highest();
-
+    first_student.highest();
+    
+    student second_student;
+    second_student.name="Aditya N. Singh";
+    second_student.age= 17;
+    second_student.roll_number=2008;
+    second_student.maths= 98;
+    second_student.physics=95;
+    second_student.highest();
 }
