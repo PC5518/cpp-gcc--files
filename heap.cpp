1e89d9642c16aaad637c6af7796a87a3ae9865ae
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-02 18:07:38 +0530

    dangling_pointer COMMIT

--- heap.cpp
+++ heap.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 int main( ) {
     int *integer= new int(421);/// IT'S A HEAP
+    // THIS IS EVEN VALID FOR OUT OF FXN
+//  ye main ke bahar bhi valid hai
+//this is the way to add a new heap based variable
     std::cout<< *integer<<std::endl;
 }
---
+++ heap22.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+	int deva=72;
+	int *pyari = new int(1);
+    delete pyari;
+	std::cout<< pyari<<std::endl; //  we make a memory access in heap ratehr than stack
+	// thing i.e to be observed is that memory adress is still the same but the
+	// data inside it is
+	std::cout<< *pyari<<std::endl;
+}
