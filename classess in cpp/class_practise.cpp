f19a8c6ad16917928281f69825291013344db1c1
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-03 16:26:19 +0530

    class final commit

---
+++ classess in cpp/class_practise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+class car {
+public:
+    int windowsize;
+    int speed;
+    char version;
+};
+int main() {
+    car ferrari;
+    ferrari.windowsize=383;
+    ferrari.speed=3893;
+    ferrari.version='a';
+    std::cout<<ferrari.speed <<std::endl;
+
+
+}
+// simple way to use class . 
