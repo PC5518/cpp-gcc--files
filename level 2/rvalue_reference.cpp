57d8f0c9895eb6460c01c16a6b95785189229584
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-03 21:00:43 +0530

    final commit for rvalue understanding. still on the way.

--- level 2/rvalue_reference.cpp
+++ level 2/rvalue_reference.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 int main() {
-    int 
+    int&& move=839;
+    // this move has no adress
+    std::cout<<move <<std::endl;
+    std::cout<<(void*)&move <<std::endl;
+    
 }
+// Do tarah ke references hain:
+// &  = lvalue reference  → cover se attach
+// && = rvalue reference  → data se attach
+// //+++==================================================
+// lvalue ⊃ rvalue
+
+// lvalue = address + data — full package 📦 int& new =x;
+// rvalue = sirf data — half package 📦 int&& reff =234;
+// rvalue i.e && ka koi bhi address nahi hota hai
+// ram me koi jagah nahi hoti hai isiliye temporary hota hai.
+// therefore && has no adress and temporary
+//===============================================
