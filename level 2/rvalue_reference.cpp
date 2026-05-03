734847f43d69e003d569da0687a02ee8b60ada4d
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-03 21:23:55 +0530

    final commit

--- level 2/rvalue_reference.cpp
+++ level 2/rvalue_reference.cpp
@@ -4,6 +4,8 @@ int main() {
     // this move has no adress
     std::cout<<move <<std::endl;
     std::cout<<(void*)&move <<std::endl;
+    5665;
+
     
 }
 // Do tarah ke references hain:
@@ -18,3 +20,8 @@ int main() {
 // ram me koi jagah nahi hoti hai isiliye temporary hota hai.
 // therefore && has no adress and temporary
 //===============================================
+// Abhi ke liye bas itna yaad rakho:
+// lvalue  = permanent — naam + address ✅
+// rvalue  = temporary — sirf data ✅
+// &       = lvalue pakdo
+// &&      = rvalue pakdo — std::move mein kaam aayega!
