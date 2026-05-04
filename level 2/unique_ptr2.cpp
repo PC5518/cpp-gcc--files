128f8598bd0c6d5610d6ccf697163c1e8fa73038
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-04 20:19:00 +0530

    unique_ptr2 understanding

---
+++ level 2/unique_ptr2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+    // with normal poimter after use you can delete it but still there's adress remains therefore memory remains.
+    // with unique ptr we can we can use it and shred it completely via .reset()
+    auto p = std::make_unique<int>(10);
+    std::cout<< &p <<std::endl;
+}
