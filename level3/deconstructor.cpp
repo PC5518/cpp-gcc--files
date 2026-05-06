f8965954825b8611b03d38011c23eb426b8d87fd
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-06 22:28:36 +0530

    deconstructor ke kaam karne kaa tarika

---
+++ level3/deconstructor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//  constructor ka kaam hota hai class ke andar present variables ki default values set karna
+// constructor== setting defaults value
+class car {
+public:
+    int code;
+    int dob;
+    std::string color;
+    car() {
+        code =0;// yaha code ya ford kisi bhi car ko default value milli 
+        dob= 0;
+        std::cout<<"Car ban gya" <<std::endl;
+    
+
+    }
+    ~car() {
+        std::cout<<"Car delete ho gyi " <<std::endl;
+    }
+};
+int main() {
+    car ford;
+    std::cout<<ford.code <<std::endl;
+}//-> deconstructor yaha call hoga
+// -> ye khali ford ka cleanup karega
+// baki ke code par koi asar nahi hoga
+// Constructor = Factory default settings 🏭
