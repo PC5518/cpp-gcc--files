31eaa49df7aa31a8324ac0b5dac7dbcaaf61ba0f
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-07 16:38:26 +0530

    final commit

---
+++ level3/copy_assignment.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+class car {
+public:
+    int code;
+    int dob;
+    std::string color;
+};
+int main() {
+    car ford;
+    ford.code=2345;
+    ford.dob = 234;
+    ford.color="Green";
+    std::cout<<ford.color <<std::endl;
+    
+    // car tata=  ford;
+    // std::cout<< tata.color<<std::endl;
+    car tata;
+    tata =ford; // alag tarike se.
+}//  NOW IF I WANNA COPY THE HYUNDAI AS BMW
+// now it's like kaam wahi lekin timing ya tarika alag alag 
