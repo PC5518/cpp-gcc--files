d361336117a516fe15a5db1420d306b9e3b30091
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-08 22:04:41 +0530

    class= defining a new type

--- level3/operator_overloading.cpp
+++ level3/operator_overloading.cpp
@@ -6,8 +6,13 @@ public:
     car(int s) { // constructor hai, parameterized constructor hai
         speed =s;
     }
+    //  now i will define  a fxn 
+    //  in reality operator is nothign but a fxn 
+    //  note:  car ek typee hai jaise int , bool etc.
+    car operator+(const car& other) {
+        return  car(speed +  other.speed);
+    }
     
-
 };
 int main( ) {
     
