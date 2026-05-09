c855e4e77cdd698cbd8ae5b2b2930932d191ee7f
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-09 22:59:01 +0530

    initialize_member understanding

---
+++ level3/initialize_member.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+class car {
+public:
+    int speed;
+    int model_number;
+    std::string name;
+    // car(int s) {
+    //     int s=speed;
+    // }
+    car(int s) : speed(s) {
+        // sedha banana with value
+    }
+
+
+};
+int main() {
+    
+}
--- level3/overallconstructor.cpp
+++ level3/overallconstructor.cpp
@@ -1,6 +1,6 @@
-Pehle problem samajh
-Abhi tu constructor mein likhta hai:
+// Pehle problem samajh
+// Abhi tu constructor mein likhta hai:
 cppcar(int s) {
     speed = s;  // pehle speed bana, phir assign kiya
 }
-Yeh do steps hain — pehle speed bana (default value ke saath), phir s assign kiya. Thoda wasteful.
+// Yeh do steps hain — pehle speed bana (default value ke saath), phir s assign kiya. Thoda wasteful.
