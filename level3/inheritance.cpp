14b785d91937004f71c46cb1d1fa11082c274966
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-10 18:08:26 +0530

    introduction to inheritance

--- level3/inheritance.cpp
+++ level3/inheritance.cpp
@@ -12,14 +12,21 @@ public:
 };
 // now mujhe ye same structure mujhe inherit karna hai for a new class i.e dog
 class Dog:  public Animal {
+public:
+    int dog_tail_length;
+private:
+    int stool_smell;
+
 //  joojo bhi hai animal me sab aa gya dog me and extra kuch jod sktee hai public me;
+// now if you want more specifically for this one
 };
 int main() {
-    std::string mumma="Happy mumma day!!!! mumma";
-    int number=0;
-    for (int i;i=1; i++) {
-        number++;
-        std::cout<<mumma <<" " << number << " times "<<std::endl;
+    Dog peru;
+    peru.age=10;
+    peru.dog_tail_length=102;
+    std::cout<< peru.age<<std::endl;
+    std::cout<< peru.dog_tail_length<<std::endl;
+    std::cout<< peru.stool_smell<<std::endl; // can't be accessed sincce it's private
 
-    }
+    // so this structure was inherited from the 
 }
