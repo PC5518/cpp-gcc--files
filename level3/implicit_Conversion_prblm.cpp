a49470f262fa3adafeb727ee441dde67b32c44ce
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-10 16:30:49 +0530

    explcit role in telling the compiler to convert it to class int only.

--- level3/implicit_Conversion_prblm.cpp
+++ level3/implicit_Conversion_prblm.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 class car {
+public:
     int speed;
-    car(int s) {
+    explicit car(int s) {
+        // yaha kabhi kabhi compiler bmw(100) me 100 ka class car samajh leta hai instead of intger
+        //  that's why we take it as 
         speed=s;
     }
+// 99% cases me explicit lagao
 };
 void drive(car c) {
     std::cout<< c.speed<< std::endl;
+}
+int main() {
+    car bmw(100);
+    drive(bmw);
 }
