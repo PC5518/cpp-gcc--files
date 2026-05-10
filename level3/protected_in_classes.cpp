296796e59904eee0f9fccfb7717051bfff0d69fa
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-10 21:09:19 +0530

    protected explained

---
+++ level3/protected_in_classes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+class vehicle {
+protected:
+    int rpm;
+    int speed;
+    int torque;
+};
+class Bike: public vehicle {
+// here we inherited all the objects from the class of vehicle
+public:
+    bool cover;
+};
+int main() {
+    Bike suzuki;
+    suzuki.speed=100;
+    std::cout<< suzuki.speed<<std::endl;
+    suzuki.cover =true;
+    std::cout<< suzuki.cover<<std::endl;
+    //  1 is same for what we say True in python 
+
+}
+// Puri copy hoti hai — public + private dono ✅
+
+// Public  → copy ✅ + access ✅
+// Private → copy ✅ + access ❌
+// Sab copy hota hai — public aur private dono. Fark sirf itna — public use kar sakte ho, private dekh nahi sakte. 🔒
+
