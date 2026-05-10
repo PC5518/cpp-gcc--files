8d10cf127aab4303ac197d3490938ac65bb2a5e0
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-11 00:57:16 +0530

    virtual fxn understanding on the way

--- level3/virtual_fxn.cpp
+++ level3/virtual_fxn.cpp
@@ -37,11 +37,16 @@ int main() {
     // normal object — dot
     Bike suzuki(100, 50, 3000, true);
     suzuki.sound();          // Bike ka sound ✅
-    vehicle *v1 = new Bike(100,50,300,true);
-    vehicle *v2= new Car(200,80,4000);
 
-    v1->sound();
-    v1->sound();
+    // pointer — arrow
+    vehicle* v1 = new Bike(100, 50, 3000, true); // we did it to create memory on heap
+    vehicle* v2 = new Car(200, 80, 4000); 
+// it's like Bike v1();..............
 
+    v1->sound();             // Bike ka sound ✅
+    v2->sound();             // Car ka sound ✅
 
+    // cleanup
+    delete v1;
+    delete v2;
 }
