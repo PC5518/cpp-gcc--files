643797c491599198aaf1b45004fbef409291bd5d
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-12 00:36:12 +0530

    virtual fxn practicse is now officiallly there

---
+++ level3/inferaces.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+class car {
+public:
+
+    virtual void printing() =0; //iska kaam hi hai pass hona and child ko dena task via ovverride
+};
+class ev: public car{
+    void  printing() override {
+        std::cout<<"We are printing under ev i.e child" <<std::endl;
+    }
+    //  yaha par jo karna hai wo karo 
+};
+int main() {
+    std::cout<< "we are under int main"<<std::endl;
+}
+// pure virtual function:
+// child classes ko force karta hai
+// ki ye function implement karna hi padega
---
+++ level3/virtual_fxn_practise.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+class aimodel {
+public :
+    int data_size;
+    int launch_data;
+    int reccursivity;
+    virtual void printing() {
+        std::cout<< "We are printing under ai model parent class "<<std::endl;
+    }
+};
+class transformer: public aimodel {
+public:
+    void printing() override {
+        std::cout<<"We are under transformer which is a child class" <<std::endl;
+    }
+};
+
+
+
+int main() {
+    aimodel* chatgpt = new transformer(); // birth point of real OOP polymorphism
+}
