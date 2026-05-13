4a7ec43e82aaa6430b575a6d2dde30e45d66124b
PC5518 <adityansdsdc@outlook.com>
2026-05-13 23:05:28 +0530

    final understanding of using this to avoid confusion of compiler

--- level_4/this.cpp
+++ level_4/this.cpp
@@ -1,4 +1,13 @@
+// the actual use of this
+//  the times when compiler can get confused betweeen variablee namee and parameters;
 #include <iostream>
+class Person {
+    int height;
+    Person(int height) {
+        this->height=height;
+
+    }
+};
 int main() {
-    std::cout<< <<std::endl;
+    //  play with your code here
 }
