e4cfd1d3b93da99b54667271af954a372217a37c
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-13 18:35:14 +0530

    default value_initialization decider for {} in classes it's very basic

--- level_4/value_initialization.cpp
+++ level_4/value_initialization.cpp
@@ -1,6 +1,15 @@
 #include <iostream >
+class index{
+public:
+    int market_cap{2005885};
+    int profit{};
+// {}->  means it's a default vvalue setter for a variable.
+};
 int main() {
     int x{};
     std::cout<< x<<std::endl;
+    index nasdaq;
+    std::cout<< nasdaq.profit <<std::endl;    
+    std::cout <<nasdaq.market_cap <<std::endl;
 }
 // {} se value zero set ho jata hai
