4d62984b5f213a6cc5c14cf0a9f8997716ebb9c4
PC5518 <adityansdsdc@outlook.com>
2026-05-13 23:35:11 +0530

    discovering the differences between heap stack and static forms of memory

--- level_4/static_test.cpp
+++ level_4/static_test.cpp
@@ -1,4 +1,17 @@
 // achha ek baat hmko samjh em aaya ki stack and heap me ek cheez common hai ki data ka existenacce only {} braces yaa scope tak hi rahta hai;
+//  lekin ye galat hai actialy Every call:
+
+// new int(0)
+
+// creates:
+
+// NEW heap memory
+
+// like:
+
+// memory block A
+// memory block B
+// memory block C
 #include <iostream>
 using namespace std;
 void tester() {
@@ -7,6 +20,7 @@ void tester() {
     cout<<(*deva ) << endl;
 }
 int main() {
+    int *deva = new int(0);
     tester() ;
     tester() ;
     tester() ;
