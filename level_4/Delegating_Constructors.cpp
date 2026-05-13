df7cbda4c13835c96b0d995cd9f8d1f4cd2421f4
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-13 19:05:40 +0530

    Delegating Constructors cpp file final understanding.

---
+++ level_4/Delegating_Constructors.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+// class person {
+// public:
+//     int age;
+//     int sevis_id;
+//     person() {
+//         age =18;
+//         sevis_id=100;
+        
+//     }
+//     person(int a) {
+//         age =a;
+//         sevis_id=100;
+
+//     }
+// };
+// in software engineering this repitetion is very bad.
+
+class person {
+public:
+    int age;
+    int sevis_id;
+    person(int a,int s )  {
+        age =a ;
+        sevis_id=s;
+    } 
+    person() {
+        person
+    }
+}
+
+int main() {
+
+}
