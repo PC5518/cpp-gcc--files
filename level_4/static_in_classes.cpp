bf0211b6938d1e550f5cad0df60bfcd34b975e51
PC5518 <adityansdsdc@outlook.com>
2026-05-14 18:24:57 +0530

    initial concept which is the base.

---
+++ level_4/static_in_classes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+class student  {
+public:
+    int marks{};
+};
+int main() {
+    student deva;
+    student aditya;
+    deva.marks=234;
+    aditya.marks=234;
+    // different objects
+}
