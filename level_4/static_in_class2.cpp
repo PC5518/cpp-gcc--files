fb9ab0503f11450a976bd21e5de990e1151f2f30
PC5518 <adityansdsdc@outlook.com>
2026-05-14 18:51:32 +0530

    understanding the error

---
+++ level_4/static_in_class2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+class student {
+public:
+    static int marks;
+    
+};
+int main() {
+    student deva;
+    student john;
+    deva.marks=234;
+    std::cout<<john.marks <<std::endl;
+}
