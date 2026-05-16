5342d2c42c8d9ef4957cce865ae55301479eadf0
PC5518 <adityansdsdc@outlook.com>
2026-05-16 13:46:57 +0530

    char factor that leds to the error in template automatic understanding of the class.

--- template_in_cpp_level_5/template5.cpp
+++ template_in_cpp_level_5/template5.cpp
@@ -28,4 +28,7 @@ int main() {
     std::cout<<fxn(2.23,3.0) <<std::endl;
     std::cout<<fxn(std::string("Aditya"),  std::string(" Narayan")) <<std::endl;
     // beatifull disaster . still in mordern c++ this ghost of ancient C remains there.
+    // actually compiler is considering char in the deep machine level
+    // that's wny it's not taking it a string;
+    // don't forget we should do itin the upcoming days ofm how we will undersytand the concept of char very deeply 
 }
