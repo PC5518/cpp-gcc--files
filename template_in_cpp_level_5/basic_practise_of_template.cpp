3850b37f7b1ee3a4555e68e4f369a0640fa19d74
PC5518 <adityansdsdc@outlook.com>
2026-05-16 18:33:55 +0530

    the same theory

--- template_in_cpp_level_5/basic_practise_of_template.cpp
+++ template_in_cpp_level_5/basic_practise_of_template.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 template <typename auto_type>
-auto_type 
+auto_type random(auto_type a, auto_type b) {
+    return a+b;
+}
+int  main() {
+    std::cout << random(12.556,566.46)<<std::endl ;
+}
+// this is how we use template to manage the conflict between the type/class in cpp. 
