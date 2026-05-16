3c7f5158c60885b3a7f66a8d2f2cd998f806288e
PC5518 <adityansdsdc@outlook.com>
2026-05-16 12:44:28 +0530

    first intro to template for the first  time

--- template_in_cpp_level_5/tempCodeRunnerFile.cpp
+++ template_in_cpp_level_5/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-
-    return a+b
+2
--- template_in_cpp_level_5/template5.cpp
+++ template_in_cpp_level_5/template5.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 // let's use  template to fix this problem  
+// so first we tell the general fxn i.e what exactly the fxn is doing irrespective of the type.
+
+template <typename type>
+type fxn(type a, type b) {
+    return a+b;
+}
+// ISME HO KYA RAHA HAI?
+// Line 1
+// template<typename T>
+
+// Meaning:
+
+// "T ek type hoga."
+
+// T koi variable nahi hai.
+
+// T represents:
+
+// int
+// double
+// float
+// string
+// vehicle
+// anythingv
+int main() {
+    std::cout<<fxn(2.23,3.0) <<std::endl;
+    std::cout<<fxn("deva", "aditya") <<std::endl;
+}
