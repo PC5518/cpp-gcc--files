a60215117b7b8a49d73fcf731bc8322239890778
PC5518 <adityansdsdc@outlook.com>
2026-05-16 12:21:34 +0530

    why  only template part 2 understanding

---
+++ template_in_cpp_level_5/template2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// int add_double(double a, double b) {
+    // return a+b;
+// } //  note it is set to return int so it;'s required to set  to double 
+double add_double(double a , double b){
+    return a+b;
+}
+int main() {
+    std::cout<< add_double(2.2,3.34) << std::endl;
+}
