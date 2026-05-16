218f3c5480e1c8d2701c2720106ab096afec7bfb
PC5518 <adityansdsdc@outlook.com>
2026-05-16 12:26:43 +0530

    CONTINUATION

--- template_in_cpp_level_5/template3.cpp
+++ template_in_cpp_level_5/template3.cpp
@@ -7,5 +7,8 @@ double add_double(double a , double b){
 }
 int main() {
     std::cout<< add_double(2.2,3.34) << std::endl;
+    std::cout<< add_double(2,3) << std::endl;
 
 }
+// NOW THIS IS HOW WE CREATE A DOUBLE FXN TO ADD IT 
+//  NOW IN TEMPLATE 4 LET'S SEE HOW TO DO STRING
