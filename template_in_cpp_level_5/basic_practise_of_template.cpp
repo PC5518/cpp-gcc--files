6240543837362b5c8c436936dd6273d89cd5b905
PC5518 <adityansdsdc@outlook.com>
2026-05-18 01:04:05 +0530

    multiple templates first time understanding

--- template_in_cpp_level_5/basic_practise_of_template.cpp
+++ template_in_cpp_level_5/basic_practise_of_template.cpp
@@ -4,6 +4,6 @@ auto_type random(auto_type a, auto_type b) {
     return a+b;
 }
 int  main() {
-    std::cout << random(12.556,566.46)<<std::endl ;
+    std::cout << random(12.55, 566.46)<<std::endl ;
 }
 // this is how we use template to manage the conflict between the type/class in cpp. 
