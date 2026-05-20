9eb454e29a27ee8b961db233016c0972a703016d
PC5518 <adityansdsdc@outlook.com>
2026-05-20 17:24:09 +0530

    the concept of placeholde:a place where a data or it's type is about to be filled may or may not  be filled by a compiler.

--- template_in_cpp_level_5/templates6.cpp
+++ template_in_cpp_level_5/templates6.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-template <typename type1,  typename type2>
+template <typename type1,  typename type2> 
+// note: here type2 and type1 are not datatype they are to be a datatype in the future 
+// that's why it's called as a placeholder
 auto fxn(type1 a, type2 b) {
     return  a+b;
 }
