9dedd6c1583e71014b80c4a0566e3b82d205e0a6
PC5518 <adityansdsdc@outlook.com>
2026-05-21 21:58:24 +0530

    why not simply call fxn ???? why only non type template question?????

---
+++ template_in_cpp_level_5/non_type_template_question.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// my biggest  question is that why not using the int n?????  i mean why not simply the normal call fxn i.e
+// this is 👇🏻
+void print(int n) {
+    std::cout<<n <<std::endl;
+}
+int main(){ 
+    std::cout<<67 <<std::endl;
+}
