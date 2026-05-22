89fa994dc9adfb9fa0efe6919a23c8518b34b269
PC5518 <adityansdsdc@outlook.com>
2026-05-22 15:08:40 +0530

    basically template specialization_best_understanding is that the compiler decides what should be the datatype must be then we specialised accordingly.

--- template_in_cpp_level_5/specialization_best_understanding.cpp
+++ template_in_cpp_level_5/specialization_best_understanding.cpp
@@ -10,3 +10,15 @@
 
 // else:
 //     generic_behavior()
+template <typename type>
+auto fxn(type value) {
+    std::cout<<value << std::endl;
+}
+// now i want to add that if the particular value is a int then print like this:
+template<>
+auto fxn<std::string>(std::string value) {
+    std::cout << "this is a string value"<<value <<std::endl;
+}
+int main() {
+    std::cout<<fxn(std::string("hey there ")) <<std::endl;
+}
