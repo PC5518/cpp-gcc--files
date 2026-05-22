144bfaf7b652a0d80a4e69b78d390019d1dd6b93
PC5518 <adityansdsdc@outlook.com>
2026-05-22 16:17:21 +0530

    clarification on where partial exists.

---
+++ template_in_cpp_level_5/a_course_lec4_explanation.cpp
@@ -0,0 +1,7 @@
+// 1. Function Template Full Specialization 
+// it does not has partial specialization
+
+// and
+
+// 2. Partial Specialization (which applies to class templates)
+//  class has both partial and full specialization;.
