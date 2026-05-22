d4c7508963857a523a40d85424ed256e9b24093b
PC5518 <adityansdsdc@outlook.com>
2026-05-22 14:37:40 +0530

    understanding specialisation

---
+++ template_in_cpp_level_5/specialization_best_understanding.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// we can get the intution of template by python:
+// Conceptually, specialization is VERY similar to:
+
+// if type(data) == int:
+//     do_this()
+
+// elif type(data) == str:
+//     do_that()
+
+// else:
+//     generic_behavior()
