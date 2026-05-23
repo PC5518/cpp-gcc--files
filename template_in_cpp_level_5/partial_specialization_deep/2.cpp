5922aa4f477f320d362d3ab48daa120df4736f5b
PC5518 <adityansdsdc@outlook.com>
2026-05-23 19:57:36 +0530

    template commit

--- template_in_cpp_level_5/partial_specialization_deep/2.cpp
+++ template_in_cpp_level_5/partial_specialization_deep/2.cpp
@@ -3,12 +3,15 @@ template <typename type>
 class car {
 public:
     type speed;
+    void show() {
+        std::cout<<speed  <<std::endl;
+    }
 };
 int main() {
     car<int> ford;
     ford.speed=234;
     car<std::string>  tata;
-    tata.speed="tata speed"
-    std::cout<< <<std ::endl;
+    tata.speed="tata speed is being prinetd";
+    std::cout<< tata.speed<<std ::endl;
 
 }
