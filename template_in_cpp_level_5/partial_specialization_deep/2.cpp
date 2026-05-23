761c262af49b20685c4924d2f3176fa908e3ce92
PC5518 <adityansdsdc@outlook.com>
2026-05-23 19:51:47 +0530

    chatgpt openai teaches the maya of template.

---
+++ template_in_cpp_level_5/partial_specialization_deep/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+template <typename type>
+class car {
+public:
+    type speed;
+};
+int main() {
+    car<int> ford;
+    ford.speed=234;
+    car<std::string>  tata;
+    tata.speed="tata speed"
+    std::cout<< <<std ::endl;
+
+}
