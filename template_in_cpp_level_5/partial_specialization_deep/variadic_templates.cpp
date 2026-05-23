50c0c86a72f9a7290e1637824eb2a81c44769565
PC5518 <adityansdsdc@outlook.com>
2026-05-23 21:27:22 +0530

    final commit

---
+++ template_in_cpp_level_5/partial_specialization_deep/variadic_templates.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Variadic Template =
+// Flexible number of parameters
+template <typename... Args>
+void print(Args... args) {
+    std::cout<< <<std::endl;
+}
+
+
+
+
+
+int main() {
+    std::cout<< <<std::endl;
+}
