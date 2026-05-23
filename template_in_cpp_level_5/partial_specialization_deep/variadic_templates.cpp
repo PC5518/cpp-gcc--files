1f7133c8a42429e95affb68dbea208ea27b2c3e8
PC5518 <adityansdsdc@outlook.com>
2026-05-23 22:17:22 +0530

    ...  ka matlab in c++ template defining

--- template_in_cpp_level_5/partial_specialization_deep/variadic_templates.cpp
+++ template_in_cpp_level_5/partial_specialization_deep/variadic_templates.cpp
@@ -5,6 +5,8 @@ template <typename... Args>
 void print(Args... args) {
     std::cout<< <<std::endl;
 }
+// ... ka matlab hua jitne bhi aur parameters aaye future me.
+// basically isi baat ka khayal rakha jata hai.
 
 // this is a template based fxn of dynamic amount of argument or parameters
 
