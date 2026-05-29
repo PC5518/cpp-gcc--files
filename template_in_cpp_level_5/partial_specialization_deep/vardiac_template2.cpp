ed78109840039ae9aad68d861e6b428fc77dcc87
PC5518 <adityansdsdc@outlook.com>
2026-05-29 22:29:48 +0530

    the issue with vardiac template compiler logic

--- template_in_cpp_level_5/partial_specialization_deep/vardiac_template2.cpp
+++ template_in_cpp_level_5/partial_specialization_deep/vardiac_template2.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 template <typename... type >
 //  FROM HERE WE START WITH THE ARGUMENT.
-void print(type... input) {
-    // write the fxn here 
+// Ye line:
+
+// template <typename... type>
+
+// compiler ko bol rahi hai:
+
+// Bhai, ek type nahi.
+
+// Types ka packet (pack) aa sakta hai.
+void print(type... input_variable) {
+    // write the fxn here
+    std::cout<< input_variable<<std::endl;
+} //  now the problem is that the compiler thinks and doubt but koan sa wala pehla wala yaa baad wala ??????
+int main() {
+    print(5);
 }
