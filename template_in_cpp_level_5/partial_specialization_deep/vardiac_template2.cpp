e4c0ef78b098349bbe80fcea7693447852bb9241
PC5518 <adityansdsdc@outlook.com>
2026-05-29 22:34:22 +0530

    core issue with the compiler's pack logic

--- template_in_cpp_level_5/partial_specialization_deep/vardiac_template2.cpp
+++ template_in_cpp_level_5/partial_specialization_deep/vardiac_template2.cpp
@@ -12,8 +12,10 @@ template <typename... type >
 // Types ka packet (pack) aa sakta hai.
 void print(type... input_variable) {
     // write the fxn here
-    std::cout<< input_variable<<std::endl;
+    std::cout<< input_variable ...<<std::endl;
 } //  now the problem is that the compiler thinks and doubt but koan sa wala pehla wala yaa baad wala ??????
+// therefore it is required to expand the package therefore we use ... after the input_variable statement
 int main() {
     print(5);
 }
+// actually the main issue is that even here compiler ko lagta hai pack of (1,22,5,0,"deva") so how can i print the pack which one exactky ?
