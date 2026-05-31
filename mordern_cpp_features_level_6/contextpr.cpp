0c3b81d179414d6cb257f8c1878e84fdb7f15387
PC5518 <adityansdsdc@outlook.com>
2026-06-01 03:18:04 +0530

    contextpr intro and understanding of compilation and runtime working rule using it

---
+++ mordern_cpp_features_level_6/contextpr.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main() { 
+    int x=154;
+    //  normally what happens is that program starts then x gets the value 5 and then so  onnnnn............
+//     What happens?
+
+// Compiler says:
+
+// Okay.
+// When the program runs,
+// I'll create x and put 5 into it.
+
+// This is called:
+
+// Runtime
+
+// The CPU will handle it when the program starts.
+// similarly
+    int deva =3+34;
+    // here also cpu calculates and store the data into it. 
+    // now when we do this thing:
+    constexpr int anscom=23+738;
+    // it means saveit and pack it into under the variable name by assigning a memory
+}
+// very imp fact
+//  accha toh iska matlab jo kaaam runtime ke time me hota hai wo kaam compilation ke time bhi ho skta hai ????
