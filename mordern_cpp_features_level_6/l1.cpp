b6bc26f4b53b559a0fdc456f84d142f7c82536d7
PC5518 <adityansdsdc@outlook.com>
2026-05-31 20:34:05 +0530

    * fix

--- mordern_cpp_features_level_6/l1.cpp
+++ mordern_cpp_features_level_6/l1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <memory>
 
 int main() {
     int* var= new int(10);
     std::cout<<*var <<std::endl;
 // this is just a normal pointer.
     auto pointer = std::make_unique<int>(1000);
-    std::cout<< pointer<<std::endl; 
+    std::cout<< *pointer<<std::endl; 
     // here in the unique pointer concept the data and the object get's deleted as the scope ends to avoid the memory leaks
 }
