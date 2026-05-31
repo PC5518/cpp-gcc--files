6a63051de3d49f2c84087ec694e5549d986856a3
PC5518 <adityansdsdc@outlook.com>
2026-05-31 20:22:12 +0530

    reviisiting memeory leaks issue and it's solution with std::make_unique along with the scope analysis

--- mordern_cpp_features_level_6/l1.cpp
+++ mordern_cpp_features_level_6/l1.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout<<*var <<std::endl;
 // this is just a normal pointer.
     auto pointer = std::make_unique<int>(1000);
-    std::cout<< pointer<<std::endl;
+    std::cout<< pointer<<std::endl; 
+    // here in the unique pointer concept the data and the object get's deleted as the scope ends to avoid the memory leaks
 }
