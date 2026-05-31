cb41d7e2de6c314772ea6be704b86b7f038cc4e1
PC5518 <adityansdsdc@outlook.com>
2026-06-01 01:35:10 +0530

    **core issue**: oh yeah type change ho rha hai gaur se dekhe toh ye compiler aese kar nahin sakta directly tradtional cpp me right ????/. this is the core issue ???/ with unions ????

--- mordern_cpp_features_level_6/unions.cpp
+++ mordern_cpp_features_level_6/unions.cpp
@@ -4,12 +4,13 @@ union data {
     int number;
     // std::string datanumber; this is creating problems
     // cuz historically union was maded for simple data types
-    int number2;
+    float number2;
 };
 int main() {
     data Xiaomi;
     Xiaomi.number=12;
     Xiaomi.number2=454152;    
+// oh yeah type change ho rha hai gaur se dekhe toh ye compiler aese kar nahin sakta directly tradtional cpp me right ????/. this is the core issue ???/ with unions ????
 
     std::cout<< Xiaomi.number<<std::endl;
     std::cout<< Xiaomi.number2 << "  this is number 2"<<std::endl;
