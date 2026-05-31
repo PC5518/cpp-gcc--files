62dba761a775d77ae3510510a85ea5628379383b
PC5518 <adityansdsdc@outlook.com>
2026-06-01 02:16:44 +0530

    variant issue at 2:16 am 1 june

--- mordern_cpp_features_level_6/std_variant.cpp
+++ mordern_cpp_features_level_6/std_variant.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-union Data
+#include <variant>
+std::variant<int, float> data;
+union data
 {
     int number;
     float value;
 };
 // now the problem is that union stores the memory in form of bits but not the datatype. so it does not store the dattype along with it.
 // that's why std::variant is used.
+// so here's the mordern c++  solution. 
+int main(){ 
+    data random;
+    random.number=521;
+    std::cout<< random.number<<std::endl;
+}   
