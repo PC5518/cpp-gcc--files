6f950e7536be7c89a638bedfe10a38f9cd359b6e
PC5518 <adityansdsdc@outlook.com>
2026-06-01 01:32:40 +0530

    core problems with unions in cpp

--- mordern_cpp_features_level_6/unions.cpp
+++ mordern_cpp_features_level_6/unions.cpp
@@ -9,7 +9,8 @@ union data {
 int main() {
     data Xiaomi;
     Xiaomi.number=12;
+    Xiaomi.number2=454152;    
 
     std::cout<< Xiaomi.number<<std::endl;
-    std::cout<< Xiaomi.number2 << "this is number 2"<<std::endl;
+    std::cout<< Xiaomi.number2 << "  this is number 2"<<std::endl;
 }
