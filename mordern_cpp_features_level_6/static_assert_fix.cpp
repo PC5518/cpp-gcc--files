d7213c94e15a6dd28633ac42d40698cad18ffeb1
PC5518 <adityansdsdc@outlook.com>
2026-06-01 18:37:16 +0530

    contribution of constexpr in static_assertion code (or the way of dealing with assert in compilation phase only)

--- mordern_cpp_features_level_6/static_assert_fix.cpp
+++ mordern_cpp_features_level_6/static_assert_fix.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <cassert>
-
 int main() {
-    int age=-7;
+    constexpr int age=-7;
     static_assert(age>=0);
 //  the only difference is that  it will run at the time of compilation only. 
 // that the main difference of proccessing and working
@@ -10,3 +9,5 @@ int main() {
 // now at the time of compilation the compiler still lacks the knowledge of whethere age is existing or not.
 //  therefore hame at the time of compile time hi age ka data dena hoga jiske bare me condtion bataya gya hai .
 // we will see the fix in the next static_assert_fix.cpp file
+// now heres; the fix
+// this is the way to make the compiler understand and deal in pre runtime and compilation phase.
