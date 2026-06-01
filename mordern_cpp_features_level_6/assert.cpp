937f90d962fe57530a754c68ff24375f28630ab7
PC5518 <adityansdsdc@outlook.com>
2026-06-01 18:00:16 +0530

    additoonal notes assigned in own language

--- mordern_cpp_features_level_6/assert.cpp
+++ mordern_cpp_features_level_6/assert.cpp
@@ -10,3 +10,5 @@ int main() {
     // to let the program know basic things before execution we assert it.
 }
 // here we can use assert the compiler for obvious things.
+// in simple words assert ke through mai strict requiremnt set karta hun for the variable.
+// it will crash fi the requirement are not satisfied.
