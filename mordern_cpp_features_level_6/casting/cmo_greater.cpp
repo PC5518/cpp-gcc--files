67c27653ccc2bf45f723ab42bc6c7ab6ecc1ff26
PC5518 <adityansdsdc@outlook.com>
2026-06-02 23:48:43 +0530

    git issues fix

--- mordern_cpp_features_level_6/casting/cmo_greater.cpp
+++ mordern_cpp_features_level_6/casting/cmo_greater.cpp
@@ -10,6 +10,7 @@ int main() {
     // when it comes to unsigned the compiler assuming the bits data of the negative to be positive and created an error.
     // i should see my number line representation to understand it. it's available in the one note application.
     std::cout<< std::cmp_greater(x,y)<<std::endl;
+    // it means is x greater than y ??????
 }
 // the overall leearning is that be carefull in comparing the signed and unsigned integers.
 
