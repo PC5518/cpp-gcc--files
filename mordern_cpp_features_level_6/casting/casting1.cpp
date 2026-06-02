81e7406193992dc2c012a72b4e481052f6029b0c
PC5518 <adityansdsdc@outlook.com>
2026-06-02 12:55:23 +0530

    implicit conversioons often took place automatically by the cpp compiler

--- mordern_cpp_features_level_6/casting/casting1.cpp
+++ mordern_cpp_features_level_6/casting/casting1.cpp
@@ -6,4 +6,12 @@ int main() {
     // let's see the core compiler rule in cpp in converting it. 
     int x=  (int)e ;
     std::cout<< x<<std::endl;
+    // now when it comes to the compiler it does not behave like c++;  
+    // compiler does not convert like python;
+    //in some cases it do behave and applies implicit conversions for example between  int and double;
+    // for example:
+    double decimal=3.14;
+    int integer= decimal;
+    std::cout<<integer <<std::endl; // here the compiler automatically takes the decimal part and performs the implicit conversions.
+    // implicit conversion took place
 }
