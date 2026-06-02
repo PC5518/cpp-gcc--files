266c64cf3b888da910a785b090ff3ab06ce07da3
PC5518 <adityansdsdc@outlook.com>
2026-06-02 18:29:14 +0530

    reason behind the compiler's uimsigned int misinterpretation in evaluating equalities.

--- mordern_cpp_features_level_6/casting/cmo_greater.cpp
+++ mordern_cpp_features_level_6/casting/cmo_greater.cpp
@@ -7,4 +7,7 @@ int main() {
 // now it's assuming that that x>y  why ??? because somewhere cpp treats differently "unsigned"
     // what does computer stores??? 
     // it's storing bits
+    // when it comes to unsigned the compiler assuming the bits data of the negative to be positive and created an error.
+    // i should see my number line representation to understand it. it's available in the one note application.
+    
 }
