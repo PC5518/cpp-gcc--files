355d1decdd763c7901b8b1cecc0975340759299c
PC5518 <adityansdsdc@outlook.com>
2026-06-02 17:38:10 +0530

    explanation of unsigned in mordern  c++

--- mordern_cpp_features_level_6/casting/unsigned.cpp
+++ mordern_cpp_features_level_6/casting/unsigned.cpp
@@ -7,5 +7,9 @@ int main(){
     unsigned int y =234;
     // this can only store positivvee or non negative values.
     // it's range goes from zero to 4294967295
-     
 }
+// now actually what happens is that when we normally write int x=33;
+// is that the plus minus symbol itself occuppies the space of 1 bit.
+// int itself is of 4 byte and 1 byte has 4 bits of memory therefore total is of 32 bits
+// in order to increase the range of an standard int , unsigned makes the negative part removed and the positive range increases
+// it's very easy to determine it. 
