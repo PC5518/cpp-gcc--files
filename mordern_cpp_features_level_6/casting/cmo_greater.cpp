2c6aa488d76ba4c6002a04f441a39df458244bcc
PC5518 <adityansdsdc@outlook.com>
2026-06-02 17:58:19 +0000

    computing way of comparing.

--- mordern_cpp_features_level_6/casting/cmo_greater.cpp
+++ mordern_cpp_features_level_6/casting/cmo_greater.cpp
@@ -6,9 +6,10 @@ int main() {
     std::cout<<(x>y) <<std::endl;
 // now it's assuming that that x>y  why ??? because somewhere cpp treats differently "unsigned"
     // what does computer stores??? 
-    // it's storing bits
+    // it's storing bits. it comapares by bits matching and comparing.
     // when it comes to unsigned the compiler assuming the bits data of the negative to be positive and created an error.
     // i should see my number line representation to understand it. it's available in the one note application.
     std::cout<< std::cmp_greater(x,y)<<std::endl;
 }
 // the overall leearning is that be carefull in comparing the signed and unsigned integers.
+
