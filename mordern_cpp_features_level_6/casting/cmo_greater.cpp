b9819406f99a8aee22d9779c6bb9741cf507cfa8
PC5518 <adityansdsdc@outlook.com>
2026-06-02 19:37:21 +0530

    ubuntu fix for wall and g++ 20 version

--- mordern_cpp_features_level_6/casting/cmo_greater.cpp
+++ mordern_cpp_features_level_6/casting/cmo_greater.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-using namespace std;
+#include <utility> // very important to correct the compiler misintepretation.
 int main() {
     int x =-23;
     unsigned int y =4541;
-    cout<<(x>y) <<endl;
+    std::cout<<(x>y) <<std::endl;
 // now it's assuming that that x>y  why ??? because somewhere cpp treats differently "unsigned"
     // what does computer stores??? 
     // it's storing bits
     // when it comes to unsigned the compiler assuming the bits data of the negative to be positive and created an error.
     // i should see my number line representation to understand it. it's available in the one note application.
-    
+    std::cout<< std::cmp_greater(x,y)<<std::endl;
 }
+// the overall leearning is that be carefull in comparing the signed and unsigned integers.
