929f2deae5c632f0cf1be68117c118cb39d16049
PC5518 <adityansdsdc@outlook.com>
2026-06-02 17:51:47 +0530

    experiment regarding the bit theory in c++ and understanding the backend of the computer at the computing runtime.

---
+++ mordern_cpp_features_level_6/casting/cmo_greater.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x =-23;
+    unsigned int y =4541;
+    cout<<(x>y) <<endl;
+// now it's assuming that that x>y  why ??? because somewhere cpp treats differently "unsigned"
+    // what does computer stores??? 
+    // it's storing bits
+}
