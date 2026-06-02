a95a6b56dda4b6059e41f44811a4573bda26d171
PC5518 <adityansdsdc@outlook.com>
2026-06-02 13:13:25 +0530

    casting basics to the other types.

--- mordern_cpp_features_level_6/casting/explicit_conversion.cpp
+++ mordern_cpp_features_level_6/casting/explicit_conversion.cpp
@@ -5,3 +5,8 @@ int main() {
     int y= (int)x; // the old style c way to convert
     cout<< y <<endl;
 }
+// Casting Basics         ⭐⭐⭐⭐⭐
+// static_cast            ⭐⭐⭐⭐⭐
+// dynamic_cast           ⭐⭐⭐
+// reinterpret_cast       ⭐⭐
+// these are the types of cast we will see
