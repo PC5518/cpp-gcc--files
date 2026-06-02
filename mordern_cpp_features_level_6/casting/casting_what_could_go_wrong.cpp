d6d0b90ca25dfbd14fcaf992622a13568556871e
PC5518 <adityansdsdc@outlook.com>
2026-06-02 17:29:01 +0530

    adding unsigned before int x=234; logic behind it

---
+++ mordern_cpp_features_level_6/casting/casting_what_could_go_wrong.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+// now in tradtion old c style casting what could go wrong and may or may not cause error.  !!!!
+int main() {
+    int x =-2;
+    
+}
---
+++ mordern_cpp_features_level_6/casting/unsigned.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){ 
+    int x =23;
+    // this was just a normal integer
+    // that can store from 2147483648 to -2147483648
+    // now when we store unsigned integer 
+    unsigned int y =234;
+    // this can only store positivvee or non negative values.
+    // it's range goes from zero to 4294967295
+     
+}
