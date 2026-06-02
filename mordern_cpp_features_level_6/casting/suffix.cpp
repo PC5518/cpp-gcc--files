3677428553e26c12895e3394024ec74dfc05112b
PC5518 <adityansdsdc@outlook.com>
2026-06-03 01:24:52 +0530

    suffix .cpp

---
+++ mordern_cpp_features_level_6/casting/suffix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    int x =23;
+    //int -> declares the type and 23 assign the value
+    // let's see what if suffix is there
+    // eg.  100u this tells it's unsigned or non negative value storer. 
+    int x =100u;
+    // this is equal to 
+    // now but the thoughtprocess behind the compiler is different 
+    unsigned int x=  100;
+
+
+}
