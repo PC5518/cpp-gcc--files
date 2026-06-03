289488c44055701047e746f6c6d2bdbc05497dfa
PC5518 <adityansdsdc@outlook.com>
2026-06-03 20:34:35 +0530

    intro to sort fxn which is the part of std. very imp

---
+++ level 7/STL FUNCTIONS/sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> array ={1,882,5,2,56,5500,4};
+    sort(array.begin(), array.end()); // begin and end means vector ke first and end element ki position.
+    //  so fxn is sorted
+    for (int i :  array) {
+        cout<< i
+        <<" ";
+    }
+}
---
+++ level 7/lambda.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int sqaure(int a ) {
+    return a*a;
+}
+int main() {
+    std::cout<<  sqaure(2);
+}
+int main() {
+    
+}
