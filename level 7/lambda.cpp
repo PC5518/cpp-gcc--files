433cf976bc83ba4dea5843a1661c5a4661ff1cff
PC5518 <adityansdsdc@outlook.com>
2026-06-03 21:07:43 +0530

    intro to lamda and first look to lambda

--- level 7/lambda.cpp
+++ level 7/lambda.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 int sqaure(int a ) {
     return a*a;
-}
+} // let's say mujhe fxn ek hi baar use karna hai lekin likhna pada
+// toh mai kya kar sakta hun ????  
+// we can write the lambda version
+// [](int a, int b) {
+//     return a+b;
+// }
+// carefully observe the fxn name has been erased
+// but how we will call it ??? 
 int main() {
-    std::cout<<  sqaure(2);
+    // std::cout<<  sqaure(2);
+    std::cout<< [](int a, int b) {
+        return  a+b;
+    }(1,3);
 }
-int main() {
-    
-}
+// this is lambda . just or simply a one time fxn use way
+// very easy/ remember it!!!  iteration.
