aa560af91be5957b0bfa07b21cfd1e3dfa6cfead
PC5518 <adityansdsdc@outlook.com>
2026-06-04 19:45:57 +0530

    name commit

--- level 7/mandm_rule.cpp
+++ level 7/mandm_rule.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 class student {
-    public: 
+public: 
     int marks=23;
-    void increase() const {
+    void increase()  {
         marks++;
     }
 
 };
 int main() {
     const student s;
+    s.increase();
     // s.marks=34; // now it;s immuttable
 // error ! 
 // now kahani me twist
