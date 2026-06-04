15e28e3cf6fbe264f7a76d7b11ab954c075582a8
PC5518 <adityansdsdc@outlook.com>
2026-06-04 20:09:32 +0530

    Big rule: In C++, a const object can only call const member functions.

--- level 7/mandm_rule.cpp
+++ level 7/mandm_rule.cpp
@@ -5,11 +5,19 @@ public:
     void increase()  {
         marks++;
     }
+    // now when it comes to 
+    void show() const {
+        std::cout<<marks <<std ::endl;
+    }
 
 };
 int main() {
-    const student s;
-    s.increase();
+    const student s; // compiler ka logic:  ye object abhi read only mode me hi hai  
+    // s.increase(); illegal !
+    s.show(); 
+    // now why is it not workng without const after show()? ???
+    // reason:  because In C++, a const object can only call const member functions.
+    // when object s was in read only mode it means
     // s.marks=34; // now it;s immuttable
 // error ! 
 // now kahani me twist
