f43d3a863f60983ecc340487374dfa87ed073764
PC5518 <adityansdsdc@outlook.com>
2026-06-04 20:57:55 +0530

    experimental: using of reference and checking how it cab be played in lambda to edit external variable by accessing their main adress jeenjow

---
+++ level 7/lambda4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// moving to the advance level of lambda fxn 
+int main() {
+    int x =10;
+    auto fxn =  [&x](int y) {
+        x=  x+y; ; // now this is error becaause lamda ke anda x exist hi nahi karta. 
+    };
+    fxn(7); // so lambda can also edit the variable using reference.
+    // withoout reference (&) it was just like the access to the data.  but after reference it can access the adress and perform edit to the actual fxn;
+    std::cout<<  x;
+}
+// bahar walo ki khabar lambda me lane ke liye we use that list portion and insert the variables and data
