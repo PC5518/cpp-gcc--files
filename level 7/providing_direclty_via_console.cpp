5334e5266f89f1d272c78f741fdd1510cd253efb
PC5518 <adityansdsdc@outlook.com>
2026-06-05 20:27:25 +0530

    narayan learns first time how to use the terminal to directly enter inputs via the terminal.

--- level 7/providing_direclty_via_console.cpp
+++ level 7/providing_direclty_via_console.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
-int main() {
-    std::cout<< "deva is printed"<<std::endl;
+int main(int number, char* argv[]) { // isme sab string ke hi roop me aata hai  by default
+    for (int i =0;i< number; i++) {
+        std::cout<<argv[i] <<std::endl;
+    }
 }
