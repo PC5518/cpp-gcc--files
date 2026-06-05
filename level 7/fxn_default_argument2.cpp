d4fb20102efca26dfcadedf4472464da250cd2bc
PC5518 <adityansdsdc@outlook.com>
2026-06-05 22:08:31 +0530

    now how manually and by default we can set the default value of the parameter in c++;

---
+++ level 7/fxn_default_argument2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int sum(int a,  int b=10) { // b has it's default value set already
+    // it will change only when the coder will change the b
+
+    return a+b;
+}
+int main() {
+    cout << sum(1) <<endl; // now here b has it's default value set to be 10
+    cout<< sum(1,123) <<endl; // now manually the value has been provided
+} 
