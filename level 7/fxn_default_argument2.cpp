268a3769dbc81d4a2a57c42991cdfb914611aabf
PC5518 <adityansdsdc@outlook.com>
2026-06-05 22:21:21 +0530

    skipping of the define part.

--- level 7/fxn_default_argument2.cpp
+++ level 7/fxn_default_argument2.cpp
@@ -9,4 +9,4 @@ int sum(int a,  int b=10) { // b has it's default value set already
 int main() {
     cout << sum(1) <<endl; // now here b has it's default value set to be 10
     cout<< sum(1,123) <<endl; // now manually the value has been provided
-} 
+}
