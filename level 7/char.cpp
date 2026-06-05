3a74e693c0146e9cc1adbde9fd9c5298aee8cdba
PC5518 <adityansdsdc@outlook.com>
2026-06-05 20:42:25 +0530

    argument count and argument vector

---
+++ level 7/char.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()  {
+    char var ='a';
+    cout <<var <<endl;
+
+}
--- level 7/providing_direclty_via_console.cpp
+++ level 7/providing_direclty_via_console.cpp
@@ -4,3 +4,6 @@ int main(int number, char* argv[]) { // isme sab string ke hi roop me aata hai  
         std::cout<<argv[i] <<std::endl;
     }
 }
+
+// argc ->  argument count
+// argv ->  argument vector
