9424933669074e0dd94bdfc398b3058a078589a8
PC5518 <adityansdsdc@outlook.com>
2026-06-05 22:53:19 +0530

    how inline fxn differs from normal fxn when it comes to memory

--- level 7/fxn_inlining.cpp
+++ level 7/fxn_inlining.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 inline int square(int x ) {
     return x*x;
-}
+} // now if the compiler inlines itself then conceptually it becomes:
+//  int y = sqaure(5);
+//  int y =  5*5 ; i.e directly will happen.
+int square_normal_fxn(int x ) {
+    return x*x;
+}   //  now this is a normal fxn
+    // now whenever you call this fxn cpu will make a stack frame
+    // actually fxn ke liye thodi stack memory lagti hai.
+    // and as the fxn gets over the stack frame gets destroyed
+
 int main() {
     std::cout<<square(5*5) << std::endl;
 }
