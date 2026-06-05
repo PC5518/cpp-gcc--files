7ff75f4b17ea75c90960da0a2131eaefbd16a500
PC5518 <adityansdsdc@outlook.com>
2026-06-05 20:53:07 +0530

    intro to using syntax in c++

---
+++ level 7/using_in_cpp.cpp
@@ -0,0 +1,12 @@
+#include  <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    cout<<"hey there howw are u " <<endl;
+    vector<int> array={2,23,3};
+    for (int i :array) {
+        cout<< i<<endl;
+    }
+
+}
+// now this comes with a lot of conflict
