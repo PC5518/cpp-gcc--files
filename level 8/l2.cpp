7d65ef7008ac8540045fc69c4ac2a3244add97cf
PC5518 <adityansdsdc@outlook.com>
2026-06-06 23:23:49 +0530

    latest commit for l2

--- level 8/l2.cpp
+++ level 8/l2.cpp
@@ -3,5 +3,21 @@ int main() {
     std::string string = "deva"; // for a computer string is just A series of character. like
     // to a compter string is d e v a  that is the series of character;
     //  now the fact is that the string value is backed by char at the hardware memory management level
-
+    // to understand it we can use the old c language based char list based memory allocation;
+    char list[]= "Aditya"; // now this is a list containing character
+    std::cout << list<<std::endl;
+    int deva[]= {1,3,3};
+    for (int i: deva) {
+        std::cout<< i<<std::endl;
+    }
+    char character ='a';
+    int data = 123;
+    int* ptr =&data;
+    std::cout<< *ptr<<std::endl; 
+    std::cout<<  character<< std::endl;
+    char random ='a';
+    std::cout 
 }
+// +---+---+---+---+---+---+----+
+// | A | d | i | t | y | a | \0 | this \0 means that the data list is over;
+// +---+---+---+---+---+---+----+
