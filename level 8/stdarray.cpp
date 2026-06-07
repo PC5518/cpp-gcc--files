11252ea71ec381956109853c9dc275c3346adc4c
PC5518 <adityansdsdc@outlook.com>
2026-06-07 18:58:46 +0530

    most important use where we can use stl alogorithm to sort front etc in std::array etc .......

--- level 8/stdarray.cpp
+++ level 8/stdarray.cpp
@@ -1,8 +1,19 @@
+#include <array>
 #include <iostream>
+#include <algorithm>
 int main() {
     int array[]={1,2,3,3,4,5,5,6,6,77};
     // the biggest problem with this was size nikalna was annoying
     int size=  sizeof(array)/4;
     std::cout<<size <<std::endl;
     // now again and again doing is annoying.
+    // also there were no member fxns;
+    std::array<int,5> stdarray={1556,2,33,45,5};
+    // just like vectors
+    std::cout<< stdarray.size()<<std::endl;
+    std::cout<< stdarray.front()<<std::endl;
+    std::sort(stdarray.begin(), stdarray.end());
+    for (int i: stdarray ) {
+        std::cout<< i << " ";
+    }
 }
