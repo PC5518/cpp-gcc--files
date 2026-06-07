1aaab7fa2714f14ab1217536f382d0ca9d2cdc94
PC5518 <adityansdsdc@outlook.com>
2026-06-07 18:31:40 +0530

    mordern solution to double copy of string via fxn: string_view

--- level 8/stdstringview.cpp
+++ level 8/stdstringview.cpp
@@ -2,9 +2,12 @@
 // void show(std::string name   ) {
 //     std::cout<< name<<std::endl;
 // }
-void show(std::string& name   ) { // by creating we can directly take the reference.
-    std::cout<< name<<std::endl;
-}
+// void show(std::string& name   ) { // by creating we can directly take the reference.
+//     std::cout<< name<<std::endl;
+// } // solution one
+void show(std::string_view name   ) {   // by using string_view somewhere same like references:  save kiya adress+length
+    std::cout<< name<<std::endl; // it's like only dekh rha hai 
+}   //mordern soltion
 int main() {
     std::string s ="variable created inside the std::string in the main fxn";
     show(s);
