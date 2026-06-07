2220a0ecebe338a8420ee5967615f9417d40d481
PC5518 <adityansdsdc@outlook.com>
2026-06-07 22:29:13 +0530

    intro to vector vector_capacity and it's working rule when we push back or append the vector

---
+++ STL Containers Part 1/vector_capacity.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+int main() { 
+    std::vector<int> v = {1,2,3,45,6}; 
+    std::cout<<v.capacity() <<std::endl; // vector capacity is measured in elements rather than bits or bytes
+    v.push_back(10); // the compiler adds 5 blocks extra i.e double the previous capacity
+    std::cout<<v.capacity() <<std::endl;
+    v.push_back(10); // 2nd item which is being added here and still 2 can be added more . after 5 are utilised again we have ccapcity 10 and size also 10 therefore 10 more will be added for future elements that will be added.
+    std::cout<<v.capacity() <<std::endl;
+}
