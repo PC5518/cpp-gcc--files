a7afc966daaca7e2010aec5e3baea594f897cd77
PC5518 <adityansdsdc@outlook.com>
2026-06-07 12:12:19 +0530

    l2 intro

--- level 8/l2.cpp
+++ level 8/l2.cpp
@@ -12,11 +12,13 @@ int main() {
     }
     char character ='a';
     int data = 123;
-    int* ptr =&data;
-    std::cout<< *ptr<<std::endl; 
+    // int* ptr =&data;
+    // std::cout<< *ptr<<std::endl; 
     std::cout<<  character<< std::endl;
-    char random ='a';
-    std::cout 
+    // let's come to char list
+    char* ptr = list;  // Array ka naam (list) normal variable ki tarah behave nahi karta. most expression me list automatically convert ho jata hai i.e &list[0]
+    std::cout<< ptr<<std::endl;
+    
 }
 // +---+---+---+---+---+---+----+
 // | A | d | i | t | y | a | \0 | this \0 means that the data list is over;
