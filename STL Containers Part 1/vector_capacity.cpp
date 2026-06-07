8c4e7877f8b5e45eae2270c44c5be742bdb63482
PC5518 <adityansdsdc@outlook.com>
2026-06-07 22:33:40 +0530

    excellent optimisation strategy via vector.reserve()

--- STL Containers Part 1/vector_capacity.cpp
+++ STL Containers Part 1/vector_capacity.cpp
@@ -7,5 +7,8 @@ int main() {
     std::cout<<v.capacity() <<std::endl;
     v.push_back(10); // 2nd item which is being added here and still 2 can be added more . after 5 are utilised again we have ccapcity 10 and size also 10 therefore 10 more will be added for future elements that will be added.
     std::cout<<v.capacity() <<std::endl;
+    // now i know that i will store about 1000 more elements in the future then i will reserve that memory in advance. 
+    // that's an excellent optimisation approach via reserb
+    v.reserve(100);
 }
 // ye jyada de deta in advance ki taki bar bar memory mangna naa pade.
