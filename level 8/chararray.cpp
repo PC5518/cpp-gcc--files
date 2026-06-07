2df9e6017a2bfba4799fe6f6ea8ba64d3f1b2e30
PC5518 <adityansdsdc@outlook.com>
2026-06-07 14:46:39 +0530

    char in depth experiments

--- level 8/chararray.cpp
+++ level 8/chararray.cpp
@@ -3,5 +3,10 @@ using std::cout;
 using std::endl;
 int main() {
     char array[] ={1,'s',3,4};
-    
+    char* ptr[]= {(char*)"Aditya",  (char*)"Narayan"};
+    // char* string="Aditya"; // you will see a warning:  ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings
+    // cout<< string<<endl;
+    //  so to align with mordern c++ we can add char* after
+    const char* non_editable = "Aditya";
+    cout<< non_editable<<endl;
 }
