5c98bed52ae384ef1b388b4d46da06b844520903
PC5518 <adityansdsdc@outlook.com>
2026-06-07 13:14:00 +0530

    char backened rule

---
+++ level 8/char.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main( ) {
+    char var ='A';
+    char* ptr=  &var ;
+    cout<< *ptr <<endl;
+    // this was a single character pointer
+    
+}
