cccb0142b0b1de860fc2a3b1fcae7ca99efae56b
PC5518 <adityansdsdc@outlook.com>
2026-06-08 23:29:30 +0530

    node vs list how does it relate

--- STL Containers Part 1/stdlist.cpp
+++ STL Containers Part 1/stdlist.cpp
@@ -3,5 +3,5 @@
 int main() {
     std::list<int> ={1,2,3,4,5};
     std::cout<< list <<std::endl;
-    // they are two sided in nature 
+    // they are two sided in nature . it's different from node.
 }
