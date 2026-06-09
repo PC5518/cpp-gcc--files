58642c7d8bda90e8e57294d23e50f98c0244659f
PC5518 <adityansdsdc@outlook.com>
2026-06-09 14:27:53 +0530

    intro to list operations

---
+++ STL Containers Part 1/stdlistoperation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+int main() {
+    std::list<int> lst={1,4,24,2,242};
+    lst.push_back(10); // push_front in similar form
+    for (int i: lst) {
+        std::cout<< i <<std::endl;
+        // 10 <-> 20 <-> 30<-> new node 
+        // internally
+    }
+    std::cout<<"*********************" <<std::endl;
+    lst.pop_back() ;
+    // the last node is destroyed
+    for (int i: lst) {
+        std::cout<< i <<std::endl;
+    }
+    // similarly ppop front
+    lst.pop_front();
+    std::cout<<"*********************" <<std::endl;
+    lst.pop_back() ;
+    // the last node is destroyed
+    for (int i: lst) {
+        std::cout<< i <<std::endl;
+    }
+    std::cout<< "the inital node data of then list is: "<< lst.front() << std::endl;
+
+    std::cout<< "the last node data of then list is: "<< lst.back() << std::endl;
+}
