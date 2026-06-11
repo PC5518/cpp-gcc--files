4831cec8774c5ea4109bdf474cce463648f31a17
PC5518 <adityansdsdc@outlook.com>
2026-06-11 16:30:49 +0530

    how  deque uses the block division of elements method to maintain to be fast even having push front.

--- STL Containers Part 1/stddeque.cpp
+++ STL Containers Part 1/stddeque.cpp
@@ -12,4 +12,25 @@ int main() {
     }
     // now here every  intger is shifted to the right. and this is a problem as it consumes ram.  
     std::cout<< " "<<std::endl;
+    
+    std::cout<<v.size() <<std::endl;
+    // now what e required was that i want a vector like indexing, push_front and push_back;
+    std::deque<int> d;
+    d.push_back(101);
+    d.push_front(1);
+    // but how does it makes the push_front faster;
+    // so the thing is that  don't think is that it's an one giant array;
+    // imagine several small arrays 
+    // block a {1,2}; block b {5,24}; and so on .....
+    // now how's these blocks are connected in a deque ????
+    // first of all there's not any pointer as deque is not a linked list. 
+    // it's connected by the concept of map. we will learn it soon
+    // so the thing is that it has a fast both push front and back
+    // what if      deque becomes full or large????
+    // it automatically again forms more blocks in it .
+    for (int i: d) {
+        std::cout<< i<< " ";
+    }
+    std::cout<<" " <<std::endl ;
+    std::cout<< d[1]<<std::endl;
 }
