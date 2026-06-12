b234d9c28eeda7415d18d4db493c75c5ca3f125e
PC5518 <adityansdsdc@outlook.com>
2026-06-12 23:18:11 +0530

    std::get interpret as object_name.first

--- STL Containers Part 2/std_get.cpp
+++ STL Containers Part 2/std_get.cpp
@@ -3,5 +3,5 @@ int main() {
     std::pair<std::string, int> student  {"Aditya" ,90};
     std::cout << student.first<<std::endl;
     // now there's one more way to get it out:
-    std::cout << std::get<0>(student)<<std::endl; 
+    std::cout << std::get<0>(student)<<std::endl; // it works as a index
 }
