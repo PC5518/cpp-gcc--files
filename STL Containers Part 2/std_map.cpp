2fd47717d1fa004e0698b97097939d1e800d32ad
PC5518 <adityansdsdc@outlook.com>
2026-06-13 00:37:39 +0530

    final records for 12th june India 2026

--- STL Containers Part 2/std_map.cpp
+++ STL Containers Part 2/std_map.cpp
@@ -3,6 +3,11 @@
 int main() {
     std::map<std::string, int > dictionary;
     dictionary["string"]=23;
-    std::cout << dictionary["string"];
+    std::cout << dictionary["string"]<<std::endl;
+    std::map<std::string, int> data{{"deva",79}, {"sangeeta",23},  {"deepak", 98}};
+    std::cout<<data["deva"] <<std::endl;
+    data["ashika"]=89;
+    data["aditya"]=28;
+    
 
 }
