fc696517ae2af1fdbd51625b2bf83d649976e5e0
PC5518 <adityansdsdc@outlook.com>
2026-06-12 23:13:43 +0530

    intro to std::get

--- STL Containers Part 2/std_pair.cpp
+++ STL Containers Part 2/std_pair.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 int main() {
-    
+    std::pair<std::string, int> student  {"Aditya" ,90};
+    std::cout << student.first<<std::endl;
 }
+
+
+
+// Memory intuition:
+
+// student
+// │
+// ├── first  -> "Aditya"
+// └── second -> 20
