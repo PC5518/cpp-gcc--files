db01c0960ffd05d5f586791d3f465672154caba0
PC5518 <adityansdsdc@outlook.com>
2026-06-13 22:10:29 +0530

    basic core reason why even the categories exist.

---
+++ ITERATORS/iterators_categories.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// First principle: iterator = generelised pointer. but not exactly a pointer.
+// why only the categories need ???
+// in vector as a container we can do indexation but in linked list it;'s not possible because it is a binary search tree not a  
+// that's wht it was important to build the categories
+// a linked list must walk node by node
+
+// that's the reason why a vector iterator iterator can do a lot of things but a linked list like std::list can do fewer things.
+
+int main() {
+    
+}
