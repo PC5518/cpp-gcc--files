ab40824f9aa678febc2e95884da9860d390593e3
PC5518 <adityansdsdc@outlook.com>
2026-06-13 22:01:59 +0530

    updated iterator

--- ITERATORS/it.cpp
+++ ITERATORS/it.cpp
@@ -4,10 +4,10 @@ int main() {
     std::vector<int> v={2,23,42,234,22,24,42};
 
     auto it= v.begin();
-    std::cout<< *it <<std::endl;
+    std::cout<< *it <<std::endl; // exactly like a pointer
     ++it;
     std::cout<< *it <<std::endl;
-    // so literally this iterators moves across the whole vector array.
+    // so literally this iterators moves across the whole vector array or the elements of a container.
     // for me as a 2 months beginner i am assuming it as an special type whicch can roa around and walk .
     // iterator has it's own iterator type. it may seem like  a pointer but it's not a pointer.
 }
