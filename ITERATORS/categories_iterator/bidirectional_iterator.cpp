07ce40053456374164493f5c7897a592511b245f
PC5518 <adityansdsdc@outlook.com>
2026-06-14 18:46:29 +0530

    update on bidirectional_iterator

--- ITERATORS/categories_iterator/bidirectional_iterator.cpp
+++ ITERATORS/categories_iterator/bidirectional_iterator.cpp
@@ -5,5 +5,5 @@
 // std::map
 // std::multiset
 // std::multimap
-// backward operation is regarded with "--it"
+// backward operation is regarded with "--it" and "++it" for the forward
 // 10 ↔ 20 ↔ 30 ↔ 40 iterator can move both front and back
