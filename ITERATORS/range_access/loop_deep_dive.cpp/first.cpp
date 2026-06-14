0dfba3ce3d174923c5693b536a0d25849a07393c
PC5518 <adityansdsdc@outlook.com>
2026-06-14 19:56:12 +0530

    updating the first approach remarking with comments for my own understanding.

--- ITERATORS/range_access/loop_deep_dive.cpp/first.cpp
+++ ITERATORS/range_access/loop_deep_dive.cpp/first.cpp
@@ -6,7 +6,7 @@ using std::endl;
 int main() {
     std::vector<int> v= {2,34,623,32,4213,564,3};
     // cout<< v.size()<< endl;
-    auto it = v.begin();
+    auto it = v.begin(); // starting with the first element
     for (int  i= 1 ; i< 5; i++) {
         std::cout<<*it <<std::endl;
         ++it;
