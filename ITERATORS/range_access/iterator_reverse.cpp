55a0cb874dcf237f8f5f3edd0d4549a135034ac5
PC5518 <adityansdsdc@outlook.com>
2026-06-14 20:48:54 +0530

    the reverse analogy of an iterator_reverse like r.begin()  r.end()

---
+++ ITERATORS/range_access/iterator_reverse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using std::cout; 
+using std::endl; 
+int main() {
+    std::vector<int>  v=  {2,23,2,24,2322};
+    auto rit=  v.rbegin(); // start from 2322 and will consider it as a beginning.
+    for (auto i = v.rbegin(); i != v.rend();++i ) { // loop says initital condtion and condition to end the loop.
+        cout<<*i << " ";
+    }
+    // 2322 24 2 23 2  now the iterator will see like this.
+
+}
