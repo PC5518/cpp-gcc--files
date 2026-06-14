d0dd46ed12b34e4e2817e411d0b3deb05a88d5b3
PC5518 <adityansdsdc@outlook.com>
2026-06-14 18:46:01 +0530

    intro to bidirectional_iterator

---
+++ ITERATORS/categories_iterator/bidirectional_iterator.cpp
@@ -0,0 +1,9 @@
+// we can move both front and back. std containers include the following.
+
+// std::list
+// std::set
+// std::map
+// std::multiset
+// std::multimap
+// backward operation is regarded with "--it"
+// 10 ↔ 20 ↔ 30 ↔ 40 iterator can move both front and back
