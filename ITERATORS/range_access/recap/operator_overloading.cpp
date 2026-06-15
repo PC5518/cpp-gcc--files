4ae56ecf2da455dc1bd9aaa5f61c12ceaa346d81
PC5518 <adityansdsdc@outlook.com>
2026-06-15 15:37:59 +0530

    the member fxn existense nessesity role in this.

--- ITERATORS/range_access/recap/operator_overloading.cpp
+++ ITERATORS/range_access/recap/operator_overloading.cpp
@@ -4,6 +4,7 @@ using std::endl;
 class youtube {
 public:
     int users;
+    int likes;
     youtube(int s ) {
         users= s;
     }
@@ -22,5 +23,8 @@ int main() {
     pycraft.display();
     cout<<pycraft.users <<std::endl;
     ++pycraft.users;
-    cout<<pycraft.users <<std::endl;
+    cout<<pycraft.users <<std::endl; // fxn baar baar yahi check karega ki object ke under me yaa iska koi member fxns hai yaa nahi with the name users.
+    ++pycraft.likes;
+
+    cout<<pycraft.likes <<std::endl;
 }
