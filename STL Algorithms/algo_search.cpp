d22d62587b7a26e014bbf10931968a8e6a695285
PC5518 <adityansdsdc@outlook.com>
2026-06-15 19:46:09 +0530

    updated concerns regardign std::find

--- STL Algorithms/algo_search.cpp
+++ STL Algorithms/algo_search.cpp
@@ -6,6 +6,6 @@ int main() {
     std::vector<int> v={10,20,30,40};
     auto it = std::find(v.begin(), v.end(), 30);
     std::cout << (void*)&it<<std::endl;
-    // toh aese iterator return ho kar hme 30 ka adress pata chal gya. 
-
+    // WARNIG:  THE ADRESS WHICH IS BEIGN PRINTED IS THE ADRESS OF THE ITERATOR ITSELF, NOT 30.
+    // overall find is used to handle a value
 }
