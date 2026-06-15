7e0e57e08f35aec63346fd6f1b0a447d81f9601e
PC5518 <adityansdsdc@outlook.com>
2026-06-15 19:31:05 +0530

    std::find Algorithm use. upddated

--- STL Algorithms/algo_search.cpp
+++ STL Algorithms/algo_search.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
     std::vector<int> v={10,20,30,40};
     auto it = std::find(v.begin(), v.end(), 30);
-
+    std::cout << (void*)&it<<std::endl;
+    // toh aese iterator return ho kar hme 30 ka adress pata chal gya. 
 
 }
