4acc1e5a8c3eecdb08904949d9693df04d8f5624
PC5518 <adityansdsdc@outlook.com>
2026-06-17 00:46:53 +0530

    intro to std::none. again trying to get the lambda intuition behind

---
+++ STL Algorithms/std_none_of.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v{2,2,34,652,32,2123};
+    bool var=  std::none_of(v.begin(), v.end(), [](int a) {
+        return a%2==0;
+    });
+    // Method 2: Move the Taskbar to the Top or Sides (Third-Party)
+    // desi langauge: "Kya sab fail hain?"
+    std::cout << var <<std::endl;
+
+}
