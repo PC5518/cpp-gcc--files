6c2daf1e551b8794ff21aa2432714d1e32e2cb0d
PC5518 <adityansdsdc@outlook.com>
2026-06-16 19:13:18 +0530

    toh yehh search karta hai like by t aking index 0 and 1 then 1 and 2 and so on for matchin g??? that's why u did [](int a, int b) and not  just int a???? toh  ye conditon lagaya

--- STL Algorithms/std_adjacent_find2.cpp
+++ STL Algorithms/std_adjacent_find2.cpp
@@ -16,6 +16,7 @@ int main() {
     std::cout<<*it <<std::endl;
     // agar kuch nahi mila then return kar dega v.end();
     // CONS : EK BAAR AAGYA PAIR THEN AAGE KA NAHI BATAYEGA
-
+    // my final reasoning: 
+    // accha toh yehh search karta hai like by t aking index 0 and 1 then 1 and 2 and so on for matchin g??? that's why u did [](int a, int b) and not  just int a???? toh  ye conditon lagaya right ?
 
 }
