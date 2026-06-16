cd88bcaf218b551bc99c3bdce0527ba51d46a420
PC5518 <adityansdsdc@outlook.com>
2026-06-16 17:55:19 +0530

    realising the error i did by using standard fxn instead of lambda.

--- STL Algorithms/find_by_trial.cpp
+++ STL Algorithms/find_by_trial.cpp
@@ -1,15 +1,14 @@
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+int check(int x) {
 
-
-
-
+    return x%2==0;
+} // just trying using normal fxns
 int main() {
     std::vector<int> v={2,3,2,24,5,22};
-    auto it =  std::find_if(v.begin(), v.end(),[](int x) {
-        return x%2==0;
-    }  );
+    auto it =  std::find_if(v.begin(), v.end(), check(*it)  );
     std::cout << *it << " whose index is " << (it- v.begin()) <<std::endl;
 
 }
