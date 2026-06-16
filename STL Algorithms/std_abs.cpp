350c7ef77181ab7892b441c2ec76158fb3266cd5
PC5518 <adityansdsdc@outlook.com>
2026-06-16 19:06:38 +0530

    absolute value Algorithms std::abs()  can be used for mod

--- STL Algorithms/std_abs.cpp
+++ STL Algorithms/std_abs.cpp
@@ -3,3 +3,4 @@
 int main() {
     std::cout << std::abs(-51)<<std::endl;
 }
+// basically this gives the absolute value of a fxn/ jyst like what i studied in the functions chapter of calculus. |x-5|
