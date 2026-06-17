be90ed4192321558e0ce3ddac357d836d019b0fb
PC5518 <adityansdsdc@outlook.com>
2026-06-18 00:10:24 +0530

    std::reversed

---
+++ STL Algorithms/std_reverse.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// Realisation: lambda bhi har ek stl algo kaa alag alag hota hai !!!!!
+// for eg:  yaha pe lambda parameters aaccept nahi karta hai 
+int main() {
+    std::vector<int> v{1,3,3443,4,2,3,42};
+    std::reverse(v.begin(),v.end() );
+    for (int i:  v) {
+        std::cout<<i <<" ";
+        // the array is reversed.
+    }
+}
+
+
+
+// std::generate_n(
+//     start_iterator,
+//     count,
+//     generator
+// );
