b4dc404d6bafee32578a87055c87ef715bf1322f
PC5518 <adityansdsdc@outlook.com>
2026-06-17 20:22:32 +0530

    testing  a sample fro std_fill_n

--- STL Algorithms/std_fill_n.cpp
+++ STL Algorithms/std_fill_n.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 int main() {
-    std::vector<int> v(10);
+    std::vector<int> v= {2,24,2,2,4,2,2,3,42,2,2};
     std::fill_n(v.begin(), 5,  100 );
     for (int i: v) {
         std::cout<< i << " ";
