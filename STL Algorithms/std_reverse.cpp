20b9b415e07d0fb4881374549c86710aa714c6f3
PC5518 <adityansdsdc@outlook.com>
2026-06-18 00:11:35 +0530

    the most important part is that these fxn modify the container. they don't return something

--- STL Algorithms/std_reverse.cpp
+++ STL Algorithms/std_reverse.cpp
@@ -7,6 +7,7 @@
 int main() {
     std::vector<int> v{1,3,3443,4,2,3,42};
     std::reverse(v.begin(),v.end() );
+    // now the most important part is that these fxn modify the container. they don't return something
     for (int i:  v) {
         std::cout<<i <<" ";
         // the array is reversed.
