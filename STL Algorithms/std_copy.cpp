ae243860d1a984d829cc73fc4dd0db693bab00e8
PC5518 <adityansdsdc@outlook.com>
2026-06-17 18:54:33 +0530

    std::copy it's used to copy from an item from vector one and export or paste it to another vector. simple idea !

--- STL Algorithms/std_copy.cpp
+++ STL Algorithms/std_copy.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <algorithm>
 int main() {
-    std::vector<int> v= {1,2,3,2,456,53,3,2};
-    std::vector<int> v1(8); // range define karna jaruri hai
-    std::copy(v.begin(), v.end(),  v1.begin());
-    for (int i:v1) {
+    std::vector<int> vector1= {1,2,3,2,456,53,3,2};
+    std::vector<int> vector2(8); // range define karna jaruri hai.  it's important to define the range
+    std::copy(vector1.begin(), vector1.end(),  vector2.begin());
+    for (int i:vector2) {
         std::cout << i<<" " ;
     }
     // now agar ek vector se dusre vector me copy krna hai toh std::copy ka usse karte hai 
