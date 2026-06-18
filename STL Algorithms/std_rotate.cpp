c2f058e481617a3400d9fc05b1a65f7cb5bddf06
PC5518 <adityansdsdc@outlook.com>
2026-06-18 18:17:49 +0530

    working rule of std::rotate stl Algorithm. my own understanding

--- STL Algorithms/std_rotate.cpp
+++ STL Algorithms/std_rotate.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <algorithm>
 int main() {
-    std::vector<int> v{1,};
+    std::vector<int> v{1,2,3,4,5,6};
+    std::rotate(v.begin(), v.begin()+2,v.end());
+    for (int i: v) {
+        std::cout<< i <<" ";
+    }
 
 }
