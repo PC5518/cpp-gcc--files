4c0c8a5b432c4bc09cad67b530a2234d931ba356
PC5518 <adityansdsdc@outlook.com>
2026-06-18 18:36:18 +0530

    std_unique: same working rule like std::remove

---
+++ STL Algorithms/std_unique.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v{2,2,2,4,2,3,2};
+    auto it= std::unique(v.begin(), v.end());
+    // now v has become:  2 4 2 3 2 3 2 
+    for (int i: v) {
+        std::cout<< i<<" ";
+    }
+    std::cout<< " " << std::endl;
+    v.erase(it, v.end());  // just like std::remove(v.begin(),v.end(), 2)  and then insert that iterator to 
+    for (int i:v) {
+        std::cout<<i <<" ";
+    }
+}
+// 😄 unique() pehli baar dekhkar lagta hai:
+
+// Ye duplicates delete karega.
+
+// Aur phir STL apna classic stunt karta hai:
+
+// Nahi.
+// Main sirf logically hataunga.
+// Physically nahi.
