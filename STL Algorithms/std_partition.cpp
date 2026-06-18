fd6acb99b950983d899ea098a97713a2df119c31
PC5518 <adityansdsdc@outlook.com>
2026-06-18 22:46:28 +0530

    narayan understands std_partition

---
+++ STL Algorithms/std_partition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v{3,2,4,2,2,34,2,2,3};
+    // Backend thinking: true wale ek taraf and false wale dusre taraf
+    std::partition(v.begin(), v.end(), [](int x) {
+        return x%2==0;
+    } );
+    for (int i: v) {
+        std::cout<<i << " ";
+    }
+    // console output: 2 2 4 2 2 34 2 3 3 
+}
