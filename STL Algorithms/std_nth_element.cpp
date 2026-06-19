a7344dd481f6e5f44d8bb5217cf03493606cda30
PC5518 <adityansdsdc@outlook.com>
2026-06-19 18:24:49 +0530

    understanding the std::nth_element Algorithm which is extremely important or cp

---
+++ STL Algorithms/std_nth_element.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// now this algorithm is very much powerful for CP!
+#include <vector>
+#include <algorithm>
+#include <string>
+int main() {
+    std::vector<int>  v{1,23,3,2,4,3453,434,3234,53,32};
+    // now let's say i want the third smallest element here.  
+    // ek toh method hai ki sort karo then via iterator or index tisre element tak jao 
+    // but why to go so long. we have this algorithmm to solve the problem:
+    std::nth_element(v.begin(), v.begin()+2, v.end());  // now the mid one is the nth element
+    auto it= v.begin();
+    for (int i=0; i<2;i++) {
+        ++it;
+    };
+    std::cout<< *it<< std::endl;
+    // ya proffesional or dimag wala method:
+    std::cout<< *(v.begin()+2);
+}
