2a5840e25af48a7ef59d84935d3ef102531e5b03
PC5518 <adityansdsdc@outlook.com>
2026-06-21 19:59:54 +0530

    std::binary_search  . requirements: then list is needed to be sorted

---
+++ STL Algorithms/std_lower_bound.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// std::lower_bound()
+// std::upper_bound()
+// std::binary_search()
+
+// All three use binary search internally.
+
+// Requirement:
+int main() {
+    // it returns an boolean value.
+    std::vector<int>  deepak{55,2,3,4,3,3,44};
+    std::sort(deepak.begin(), deepak.end());
+    bool found= std::binary_search(deepak.begin(), deepak.end(), 55);
+    std::cout<< found;
+}
