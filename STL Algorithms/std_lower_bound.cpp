7065a5befcdc7d0b45ca51649d70bf8889f5409c
PC5518 <adityansdsdc@outlook.com>
2026-06-29 13:43:59 +0530

    application of indices fetching.

--- STL Algorithms/std_lower_bound.cpp
+++ STL Algorithms/std_lower_bound.cpp
@@ -9,5 +9,6 @@ int main() {
     auto it2=std::lower_bound(vector.begin(), vector.end(),55);
     std::cout<<"45 chahiye toh 45 ko iterator ke form me return kar ke dega: " << *it <<std::endl ;
     std::cout<<"55 chahiye toh 55 se just jo sabse bada hoga wo dega i.e: " << *it2 <<std::endl ;
-
+    // now in order to get he final value of 
+    std::cout<<(it-vector.begin()) <<std::endl; // this in how in can get th indices
 }
