7f8121b1a93d46cbdc98826dfaa1b56882dc4b1b
PC5518 <adityansdsdc@outlook.com>
2026-06-30 18:06:19 +0530

    understanding thr difference

--- STL Algorithms/stl  maths algorithm/std_set_union.cpp
+++ STL Algorithms/stl  maths algorithm/std_set_union.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <vector>
+#include <set>
 #include <algorithm>
 
 int main() {
-    std::vector<int> A{1,2,3,4};
-    std::vector<int> B{3,4,5,6};
+    std::set<int> A{1,2,3,4};
+    std::set<int> B{3,4,5,6};
     std::set_union(A.begin(), A.end(), B.begin(),  B.end());
     
     
