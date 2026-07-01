d79088a475c58a874dcae7045aef48f487a2e67a
PC5518 <adityansdsdc@outlook.com>
2026-07-01 23:13:47 +0530

    another way to use std::min and max

--- STL Algorithms/std_min_and_max.cpp
+++ STL Algorithms/std_min_and_max.cpp
@@ -4,5 +4,7 @@
 int main() {
     std::vector<int> v{333,3,4,4223,422,53,24};
     std::cout<< std::min(2,3)<<std::endl; // now this can also be used with variables.
-    std::cout<< std::max(2,43);
+    std::cout<< std::max(2,43)<< std::endl;
+    int largest=  std::max({2,4,5,3,433445,33});
+    std::cout<< largest<<std::endl ;
 }
