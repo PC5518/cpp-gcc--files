73048fce5eb93944b2a8dfb172759c90ad1fc353
PC5518 <adityansdsdc@outlook.com>
2026-07-01 22:39:10 +0530

    std_min_and_max_custom_comparator

---
+++ STL Algorithms/std_min_and_max_custom_comparator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v{333,3,4,4223,422,53,24};
+    std::cout<< std::min(2,3)<<std::endl; // now this can also be used with variables.
+    std::cout<< std::max(2,43);
+}
