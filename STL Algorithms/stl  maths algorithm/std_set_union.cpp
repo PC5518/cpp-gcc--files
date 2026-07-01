8ca529c7dc9282e842ec3350582fb3a4bd291f51
PC5518 <adityansdsdc@outlook.com>
2026-07-01 18:18:28 +0530

    std::std_set_union and std::back_inserter

--- STL Algorithms/stl  maths algorithm/std_set_union.cpp
+++ STL Algorithms/stl  maths algorithm/std_set_union.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <set>
+#include <vector>
 #include <algorithm>
-
-int main() {
-    std::set<int> A{1,2,3,4};
-    std::set<int> B{3,4,5,6};
-    std::set_union(A.begin(), A.end(), B.begin(),  B.end());
-    
-    
+int main() { 
+    std::vector<int> seta{1,2,3,4,5};
+    std::vector<int> setb{4,5,6,7};
+    std::vector<int> result;
+    std::set_union(seta.begin(),  seta.end(), setb.begin(), setb.end(), std::back_inserter(result));
+    for (int i: result) {
+        std::cout << i<<std::endl;
+    };
 }
+// we use std::backinserter to show jitna use utna hi dete (push_back) jao.
