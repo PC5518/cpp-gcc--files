ee262eaf15477edeb742949b2373f952e479c943
PC5518 <adityansdsdc@outlook.com>
2026-07-04 23:09:21 +0530

    updated

---
+++ STL Algorithms/std_min_element.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    std::vector<int> v{1,24,533,45};
+    // now min max kya tha ki only two values ko compare karta tha 
+    // now this mik_element will return the minimum element as an iterator.
+    // that is now yaha jitne bhi elements ho container me.
+    
+}
--- STL Algorithms/std_minmax.cpp
+++ STL Algorithms/std_minmax.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+// std::minmax in C++ returns a std::pair where the first element is the minimum and the second element is the maximum.
 int main() {
-    
-}
+    auto p=  std::minmax(15,9, [](int a,  int b ) {
+        return a<b;
+    }); 
+    std::cout<< p.first<<std::endl ;
+} 
+// if (a<b)  then 
+// class a {
+//     int first=a;
+//     int second=b ;
+// }
