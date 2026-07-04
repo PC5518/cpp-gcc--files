4a7d474264383379e8db8c6560fa6b71b647f7d4
PC5518 <adityansdsdc@outlook.com>
2026-07-04 23:26:15 +0530

    understanding the std_minmax_element

---
+++ STL Algorithms/std_minmax_element.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// std::minmax in C++ returns a std::pair where the first element is the minimum and the second element is the maximum.
+
+int main() {
+    std::vector<int>  v{1,2,3,4,6322,334,5};
+    auto data= std::minmax_element(v.begin(), v.end());// it returns an std::pair containing two iterators.
+    std::cout<<"lowest element is" << data.first  <<std::endl;
+    std::cout<<"largest element is" << data.second  <<std::endl;
+} 
