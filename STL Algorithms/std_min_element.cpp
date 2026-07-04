3c12ccd3a7b91ba9f32f0aec4a7a85a03f3f1af6
PC5518 <adityansdsdc@outlook.com>
2026-07-04 23:11:05 +0530

    std_min_element and for max application.

--- STL Algorithms/std_min_element.cpp
+++ STL Algorithms/std_min_element.cpp
@@ -6,5 +6,10 @@ int main() {
     // now min max kya tha ki only two values ko compare karta tha 
     // now this mik_element will return the minimum element as an iterator.
     // that is now yaha jitne bhi elements ho container me.
-    
+    auto it=std::min_element(v.begin(), v.end());
+    std::cout<<(*it) <<std::endl;
+    auto it2=std::max_element(v.begin(), v.end());
+    std::cout<<(*it2) <<std::endl;   
+    // extremely used !!
+
 }
