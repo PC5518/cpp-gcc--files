52677da69fc2d88e0e90afecb5d2a7bd9bb4dedc
PC5518 <adityansdsdc@outlook.com>
2026-07-06 20:40:51 +0530

    understanding std::iota

---
+++ Math Algorithm/std_iota.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+int main() {
+    // now we fill valuees in the traditonal form like this;
+    std::vector<int>  v(4);
+    std::cout<< v.size()<<std::endl;
+    for (int i=0;  i<v.size(); i++) {
+        v[i]=3;
+    }
+    for (int i:v) {
+        std::cout<<i << " ";
+    };
+    // now the next method is to use the std::generate
+    int number=0;
+    std::generate(v.begin(), v.end(), [&number]() {
+        number++;
+        return number;
+    });
+    std::cout<< " "<< std::endl;
+    for (int i:v) {
+        std::cout<<i << " ";
+    };
+    // now the next method by numeric header is 
+    std::iota(v.begin(), v.end(), 1);
+    for (int i:v) {
+        std::cout<<i << " ";
+    };
+    // iski property hai ki ye ek baar se aage badhega 
+    
+}
