ab35cb51602975ea90f522239bdb8ff5709f8448
PC5518 <adityansdsdc@outlook.com>
2026-07-06 19:34:15 +0530

    std_midpoint working rule

---
+++ Math Algorithm/std_midpoint2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+int main() {
+    int first =234;
+    int last=244;
+    int result =std::midpoint(first, last);
+    std::cout<< result;
+    // now this calculates the middpoiny and then take the absoulte value of the result.
+}
