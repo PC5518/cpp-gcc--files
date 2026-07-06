04cd36c8376653626455ac74b7656729068d6f92
PC5518 <adityansdsdc@outlook.com>
2026-07-06 19:32:07 +0530

    narayan understands std_midpoint

--- Math Algorithm/std_midpoint.cpp
+++ Math Algorithm/std_midpoint.cpp
@@ -7,4 +7,5 @@ int main() {
     int mid=std::midpoint(8,13); // it will take the greatest intger fxn after evaluating the (a+b)/2
     std::cout<<mid <<std::endl;
     std::cout<< "Mathematical midpoint(GIF): \((8 + 13) / 2 = 21 / 2 = 10.5)Rounding direction: Rounded toward";
+    
 }
