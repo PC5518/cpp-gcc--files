14987a28d28af4e67675ef4b9de1a15e02201cda
PC5518 <adityansdsdc@outlook.com>
2026-07-06 19:38:05 +0530

    how std_midpoint saves us in ads

--- Math Algorithm/std_midpoint2.cpp
+++ Math Algorithm/std_midpoint2.cpp
@@ -9,4 +9,8 @@ int main() {
     std::cout<< result;
     // now this calculates the middpoiny and then take the absoulte value of the result.
     // it also avoids the overflow issue. like the need to write longlong and etc
+
+    // why is this important????
+    // when it comes to binary search log likhte hai:  mid =(low + high)/2;
+    // fir overflows ka issue ata hai.
 }
