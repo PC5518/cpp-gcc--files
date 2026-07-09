f03db3e8a7e3ba452771798299aa0a053c9f8f7e
PC5518 <adityansdsdc@outlook.com>
2026-07-09 23:28:01 +0530

    understandig the realtion between std::partial_sum and std::adjacent_difference

--- Math Algorithm/important_relation.cpp
+++ Math Algorithm/important_relation.cpp
@@ -6,10 +6,15 @@
 int main() {
     std::vector<int> v{1,3,5,52,343};
     std::vector<int> result(v.size());
+    std::vector<int>  original_vector(v.size()); 
     std::adjacent_difference(v.begin(), v.end(), result.begin());
     for (int i: result) {
         std::cout<< i<<" ";
     }
     std::cout<< " "<<std::endl;
-    
+    std::partial_sum(result.begin(),  result.end(), original_vector.begin());
+    std::cout<< " here we are back at eh original vector v from where we started:  ";
+    for (int i: original_vector) {
+        std::cout<< i<<" ";
+    }
 }
