bc4b07ec31758aff03d354f7aac1b7c39935e593
PC5518 <adityansdsdc@outlook.com>
2026-07-09 20:39:15 +0530

    std_adjacent_difference:  toh pehla element wwahi rahega and then next - firrst

--- Math Algorithm/std_adjacent_difference.cpp
+++ Math Algorithm/std_adjacent_difference.cpp
@@ -3,8 +3,15 @@
 #include <numeric>
 #include <algorithm>
 int main()  {
-    std::vector<int> v{1,3,682,23,56723,45};
+    std::vector<int> v{1,2,3,3,4};
     std::vector<int> result(v.size());
-    auto result  =std::adjacent_difference(v.begin(),  v.end())
-    
+    //  suppose the vector is 1,2,3 the the adjacent pairs will be (1,2), (2,3) 
+    // bas itna:  basically set creation ho rha hai.
+    // now if we take the difference 2-1=1; 3-2=1;   aage - piche wala 
+    std::adjacent_difference(v.begin(), v.end(), result.begin()  );
+    // toh pehla element wwahi rahega and then (next - firrst) 
+    for (int i: result) {
+        std::cout <<i <<" ";
+    }
+    // 1 1 1 0 1
 }
