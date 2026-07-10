bbacb86bf64377c47627062d63dfb9a0f1cc4a36
PC5518 <adityansdsdc@outlook.com>
2026-07-10 16:50:21 +0530

    sytax to std::std_transform_reduce

--- Math Algorithm/std_transform_reduce.cpp
+++ Math Algorithm/std_transform_reduce.cpp
@@ -6,8 +6,16 @@ int main() {
     // transform+reduce .  ye math algorithm dono process ko mila deta hai 
 
     std::vector<int>v{2,42,453};
-    auto result= std::transform_reduce(v.begin(), v.end(),0, [](int a) {
+    auto result= std::transform_reduce(v.begin(), v.end(),0,std::plus<>() ,[](int a) {
         return a*a;
     });
     std::cout<<result <<std::endl;
 }
+// this is the fxn signature:
+
+// std::transform_reduce(
+//     A.begin(),
+//     A.end(),
+//     B.begin(),
+//     initial
+// );
