4b6bc4329b788f98f0a623386bd1c071d7d65fd8
PC5518 <adityansdsdc@outlook.com>
2026-07-14 17:33:18 +0530

    a very small understsnding of \n this acts as an subsitutue for std::endl

---
+++ slash_n.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout<< "hey there how are you \nhope that you are doing well"<<std::endl;
+    std::cout<< "hey there how are you " << std::endl;
+    std::cout<<  "hope that you are doing well"<<std::endl;
+    return 0;
+}
