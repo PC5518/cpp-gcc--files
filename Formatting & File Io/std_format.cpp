f62378efa133bd804c8b88e5ea834f623b2f0a67
PC5518 <adityansdsdc@outlook.com>
2026-07-14 18:19:51 +0530

    toh ye placeholder is liye advanced hai because ye irrespective of data type yaa class absorb kar lega ??? right ? wahi C wala only %d for decimal andd so on  i.e messy

--- Formatting & File Io/std_format.cpp
+++ Formatting & File Io/std_format.cpp
@@ -20,6 +20,7 @@ int main() {
     << " out of "
     << total
     << ".";
+    std::cout<< " "<<std::endl;
     // this becomes extremely long and hard to rearrange
     // now the next alternative is std::stringstream
     // std::stringstream  now this is very flexiblle and very easy 
@@ -40,11 +41,14 @@ int main() {
     // std::format(...)
     //     │
     //     ▼
-
     // std::string
     // the fxn signature: 
-
-    // std::string format(
+    // std::string format(   now here std::string is the return type
     // std::format_string<Args...> fmt,
     // Args&&... args);
+    // iska pura funda simple hai that is pehle arrange properly and then return that value value as string that will be later printed on the console via console output cout
+    std::string data = std::format("Age= {}", age);  // this {} is really amazing !!!
+    std::cout<< data;
+    // my own realisation:  toh ye placeholder {} is liye advanced hai because ye irrespective of data type yaa class absorb kar lega ??? right ? wahi C wala only %d for decimal andd so on  i.e messy 
+
 }
