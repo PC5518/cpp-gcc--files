a73b4c85e847c36e4ca91dae4d8b8746554b8d26
PC5518 <adityansdsdc@outlook.com>
2026-07-14 18:04:11 +0530

    the actual purpose behind the foundation of std::format

---
+++ Formatting & File Io/std_format.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <format>
+
+int main() {
+    int age=23;
+    std::string name= " this is a name";
+    double marks = 233.44;
+    double total = 451.34;
+    printf("Age = %d \n",  age); // old fomat C style 
+    // now this %d makes a placeholder that this value as a decimal will take place here.
+    // but it's messy ugly and also type unsafe
+    // now the option two is:
+    std::cout<< "Age:" << age<< "\n" <<std::endl;
+    // now this is sade but what if ?
+    std::cout
+    << "Student "
+    << name
+    << " scored "
+    << marks
+    << " out of "
+    << total
+    << ".";
+    // this becomes extremely long and hard to rearrange
+    // now the next alternative is std::stringstream
+    // std::stringstream  now this is very flexiblle and very easy 
+    // The committee wanted something that is
+
+    // readable
+    // type-safe
+    // fast
+    // modern
+    // Python-like
+    // now the  purpose was to create a formatted string from values.
+    // notice the wording.
+    // it does not print
+    // it returns a string
+    // Input values
+    //     │
+    //     ▼
+    // std::format(...)
+    //     │
+    //     ▼
+
+    // std::string
+    
+}
