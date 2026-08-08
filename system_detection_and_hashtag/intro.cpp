20df627a3bc475a3954f8a76d18a4efc517c8ec8
PC5518 <adityansdsdc@outlook.com>
2026-08-08 13:08:26 +0530

    use of # or the lines which pre proccess and creates a clean up version of the code inside all the fxns and without "#" based on the provided pre  proess code.

---
+++ system_detection_and_hashtag/intro.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+/*  these are the c style comments accross the lines
+# ka asli kaam kya hai ?  
+yeh .cpp wala files padhta hai, # wale line dekhe ke actual actual code ko modify and replac karta hai 
+aur ek naya "clean up" version banaata hai. jo tab real compiler ko diya jata hai. matlab jab tak pre proccessor apna kaam khatam nahi karta hai atab tak compiler ne actual code like int main and other fxns ko dekha hi nahi hoga.4
+
+*/
