c8e20ac56e1641ae7d809ff7cad0bf04bfed4264
PC5518 <adityansdsdc@outlook.com>
2026-08-08 13:25:08 +0530

    how #define works .

--- system_detection_and_hashtag/intro.cpp
+++ system_detection_and_hashtag/intro.cpp
@@ -5,3 +5,14 @@ yeh .cpp wala files padhta hai, # wale line dekhe ke actual actual code ko modif
 aur ek naya "clean up" version banaata hai. jo tab real compiler ko diya jata hai. matlab jab tak pre proccessor apna kaam khatam nahi karta hai atab tak compiler ne actual code like int main and other fxns ko dekha hi nahi hoga.4
 asaan bhasha : preproceessing directives.
 */
+#define PI 3.14159
+int main() {
+    std::cout << PI <<std::endl;
+    int r;
+    std::cin>>r; 
+    double area =  PI*r*r;
+    std::cout << area<<std::endl;
+
+    // YAHA PRE PROCESSOR KE COMMANDS PE CLEAN UP KE SAMAY PI Ko literally 3.14 se replace kar deta hai. 
+
+}
