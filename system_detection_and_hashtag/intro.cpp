ec91d6d656a9abdfb4d9bd6ac681a982bea8282f
PC5518 <adityansdsdc@outlook.com>
2026-08-08 13:16:38 +0530

    asaan bhasha : preproceessing directives.

--- system_detection_and_hashtag/intro.cpp
+++ system_detection_and_hashtag/intro.cpp
@@ -3,5 +3,5 @@
 # ka asli kaam kya hai ?  
 yeh .cpp wala files padhta hai, # wale line dekhe ke actual actual code ko modify and replac karta hai 
 aur ek naya "clean up" version banaata hai. jo tab real compiler ko diya jata hai. matlab jab tak pre proccessor apna kaam khatam nahi karta hai atab tak compiler ne actual code like int main and other fxns ko dekha hi nahi hoga.4
-
+asaan bhasha : preproceessing directives.
 */
