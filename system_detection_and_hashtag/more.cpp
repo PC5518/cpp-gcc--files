6ffe7a1d1a874ddd439cd6acdf77888e7d3d6835
PC5518 <adityansdsdc@outlook.com>
2026-08-08 17:43:35 +0530

    nothing after #endif

--- system_detection_and_hashtag/more.cpp
+++ system_detection_and_hashtag/more.cpp
@@ -5,5 +5,6 @@ int main() {
 #ifdef pi 
     std::cout <<"hi there" <<std::endl;
 #endif   //  endif matlab yaha par mera mera #ifdef and #if block end ho rha hai.
+// iske baad kuch nahi aana chahiye
 
 }
