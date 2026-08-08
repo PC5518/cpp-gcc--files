6bde3dcd6b33eb1054b4ad278e4e2f8bef461166
PC5518 <adityansdsdc@outlook.com>
2026-08-08 17:44:50 +0530

    meaning of endif means if block yaha khatam ho raha hai

--- system_detection_and_hashtag/more.cpp
+++ system_detection_and_hashtag/more.cpp
@@ -6,5 +6,7 @@ int main() {
     std::cout <<"hi there" <<std::endl;
 #endif   //  endif matlab yaha par mera mera #ifdef and #if block end ho rha hai.
 // iske baad kuch nahi aana chahiye
-
+#ifdef pi          // "if pi is defined" — block SHURU
+    std::cout << "hi there";
+#endif             // "end + if" — matlab "yeh IF block yahan KHATAM"
 }
